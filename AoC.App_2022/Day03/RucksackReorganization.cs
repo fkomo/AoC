1facce9e9677594655c405a26266a4657651ae85
filip komorovsky <filip.komorovsky@gmail.com>
2022-12-03 15:12:14 +0100

    2022/03 update

--- AoC.App_2022/Day03/RucksackReorganization.cs
+++ AoC.App_2022/Day03/RucksackReorganization.cs
@@ -8,20 +8,17 @@ namespace Ujeby.AoC.App.Day03
 		{
 			DebugLine($"{ input.Length } rucksacks");
 
+			var items = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 			// part1
-			long result1 = input.Select(r => 
-			{
-				var error = r[..(r.Length / 2)].Intersect(r[(r.Length / 2)..]).Single();
-				return error >= 'a' ? error - 'a' + 1 : error - 'A' + 27;
-			}).Sum();
+			long result1 = input
+				.Select(r => items.IndexOf(r[..(r.Length / 2)].Intersect(r[(r.Length / 2)..]).Single()) + 1)
+				.Sum();
 
 			// part2
 			long result2 = 0;
 			for (var i = 0; i < input.Length; i += 3)
-			{
-				var badge = input[i].Intersect(input[i + 1]).Intersect(input[i + 2]).Single();
-				result2 += badge >= 'a' ? badge - 'a' + 1 : badge - 'A' + 27;
-			}
+				result2 += items.IndexOf(input[i].Intersect(input[i + 1]).Intersect(input[i + 2]).Single()) + 1;
 
 			return (result1, result2);
 		}
