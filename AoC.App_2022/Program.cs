44963c4e285ff5ab8a242825e71f3ff5b1900c57
filip komorovsky <filip.komorovsky@gmail.com>
2022-12-03 11:55:11 +0100

    2022/03

--- AoC.App_2022/Program.cs
+++ AoC.App_2022/Program.cs
@@ -10,7 +10,7 @@ namespace Ujeby.AoC.App
 			{
 				new Day01.CalorieCounting()			{ SolutionPart1 = 66306,	SolutionPart2 = 195292 },
 				new Day02.RockPaperScissors()		{ SolutionPart1 = 13484,	SolutionPart2 = 13433 },
-				new Day03.RucksackReorganization()  { SolutionPart1 = 8394,		SolutionPart2 = null },
+				new Day03.RucksackReorganization()  { SolutionPart1 = 8394,		SolutionPart2 = 2413 },
 
 				// TODO 2022
 			});
