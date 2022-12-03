1e4f35f1361d2d3576fd69e50efbaa2e6a45ccff
filip komorovsky <filip.komorovsky@gmail.com>
2022-12-03 11:54:37 +0100

    2022/03

---
+++ AoC.App_2022/Day03/RucksackReorganization.cs
@@ -0,0 +1,30 @@
+﻿using Ujeby.AoC.Common;
+
+namespace Ujeby.AoC.App.Day03
+{
+	internal class RucksackReorganization : ProblemBase
+	{
+		protected override (long, long) SolveProblem(string[] input)
+		{
+			DebugLine($"{ input.Length } rucksacks");
+
+			// part1
+			long result1 = 0;
+			foreach (var r in input)
+			{
+				var error = r[..(r.Length / 2)].Intersect(r[(r.Length / 2)..]).Single();
+				result1 += error >= 'a' ? error - 'a' + 1 : error - 'A' + 27;
+			}
+
+			// part2
+			long result2 = 0;
+			for (var i = 0; i < input.Length; i += 3)
+			{
+				var badge = input[i].Intersect(input[i + 1]).Intersect(input[i + 2]).Single();
+				result2 += badge >= 'a' ? badge - 'a' + 1 : badge - 'A' + 27;
+			}
+
+			return (result1, result2);
+		}
+	}
+}
--- AoC.App_2022/Program.cs
+++ AoC.App_2022/Program.cs
@@ -8,8 +8,9 @@ namespace Ujeby.AoC.App
 		{
 			AdventOfCode.Run("https://adventofcode.com/2022", new ISolvable[]
 			{
-				new Day01.CalorieCounting()			{ SolutionPart1 = 66306,    SolutionPart2 = 195292 },
-				new Day02.RockPaperScissors()		{ SolutionPart1 = 13484,    SolutionPart2 = 13433 },
+				new Day01.CalorieCounting()			{ SolutionPart1 = 66306,	SolutionPart2 = 195292 },
+				new Day02.RockPaperScissors()		{ SolutionPart1 = 13484,	SolutionPart2 = 13433 },
+				new Day03.RucksackReorganization()  { SolutionPart1 = 8394,		SolutionPart2 = null },
 
 				// TODO 2022
 			});
