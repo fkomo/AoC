999c36804a3241a9e54df5fb30c867eab49b4620
filip komorovsky <filip.komorovsky@gmail.com>
2022-12-07 08:42:50 +0100

    2021/13 update

--- AoC.App_2021/Day13/TransparentOrigami.cs
+++ AoC.App_2021/Day13/TransparentOrigami.cs
@@ -4,48 +4,29 @@ namespace Ujeby.AoC.App.Day13
 {
 	internal class TransparentOrigami : ProblemBase
 	{
-		internal struct Point
-		{
-			public int X, Y;
-		}
-
 		protected override (string, string) SolveProblem(string[] input)
 		{
-			var points = new List<Point>();
+			var points = new List<int[]>();
 			var i = 0;
 			for (; input[i].Length != 0; i++)
 			{
 				var split = input[i].Split(',');
-				points.Add(new Point { X = int.Parse(split[0]), Y = int.Parse(split[1]) });
+				points.Add(new[]{ int.Parse(split[0]), int.Parse(split[1]) });
 			}
-			var size = new Point { X = points.Max(p => p.X), Y = points.Max(p => p.Y) };
 
 			// part1
-			long? answer1 = 0;
 			var instr = input[i + 1];
 			var fold = int.Parse(instr["fold along x=".Length..]);
-			if (instr["fold along ".Length] == 'x')
-			{
-				answer1 = points.Count(p =>
-				{
-					if (p.X < fold)
-						return true;
-
-					return !points.Any(lp => lp.X == fold - (p.X - fold) && lp.Y == p.Y);
-				});
-			}
-			else //if (instr["fold along ".Length] == 'y')
-			{
-				answer1 = points.Count(p =>
-				{
-					if (p.Y < fold)
-						return true;
-
-					return !points.Any(lp => lp.X == p.X && lp.Y == fold - (p.Y - fold));
-				});
-			}
+			var axis = (instr["fold along ".Length] == 'x') ? 0 : 1;
+			long? answer1 = points.Count(
+				p => (p[axis] < fold) || !points.Any(lp => lp[axis] == fold - (p[axis] - fold) && lp[(axis + 1) % 2] == p[(axis + 1) % 2]));
 
 			// part2
+			var size = new[] { points.Max(p => p[0]) + 1, points.Max(p => p[1]) + 1 };
+			var paper = new bool[size[1], size[0]];
+			foreach (var p in points)
+				paper[p[1], p[0]] = true;
+
 			// TODO 2021/13 part2
 			long? answer2 = null;
 
