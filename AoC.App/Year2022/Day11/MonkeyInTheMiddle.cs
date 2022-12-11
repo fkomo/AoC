ad47cbfb282983105ae7010505224969da09c538
filip komorovsky <filip.komorovsky@gmail.com>
2022-12-11 13:55:23 +0100

    2022/11 refactoring

--- AoC.App/Year2022/Day11/MonkeyInTheMiddle.cs
+++ AoC.App/Year2022/Day11/MonkeyInTheMiddle.cs
@@ -12,14 +12,14 @@ namespace Ujeby.AoC.App.Year2022.Day11
 			// part2
 			var monkeys = CreateMonkeys(input).ToArray();
 			long manage = 1;
-			foreach (var m in monkeys)
-				manage *= m.test;
+			foreach (var (items, op, test, next) in monkeys)
+				manage *= test;
 			long? answer2 = MonkeyBusiness(monkeys, 10000, $"old % {manage}");
 
 			return (answer1?.ToString(), answer2?.ToString());
 		}
 
-		private static long MonkeyBusiness((List<long> items, string op, long test, int[] next, long inspects)[] monkeys, int rounds, string relief)
+		private static long MonkeyBusiness((List<long> items, string op, long test, int[] next)[] monkeys, int rounds, string relief)
 		{
 			var inspects = new long[monkeys.Length];
 
@@ -42,26 +42,25 @@ namespace Ujeby.AoC.App.Year2022.Day11
 				}
 			}
 
-			var monkeyBusiness = inspects.OrderByDescending(n => n).Take(2).ToArray();
+			var monkeyBusiness = inspects.OrderByDescending(n => n).ToArray();
 			return monkeyBusiness[0] * monkeyBusiness[1];
 		}
 
-		private static List<(List<long> items, string op, long test, int[] next, long inspects)> CreateMonkeys(string[] input)
+		private static List<(List<long> items, string op, long test, int[] next)> CreateMonkeys(string[] input)
 		{
-			var monkeys = new List<(List<long> items, string op, long test, int[] next, long inspects)>();
+			var monkeys = new List<(List<long> items, string op, long test, int[] next)>();
 			for (var l = 0; l < input.Length; l += 7)
 			{
 				monkeys.Add(
 					(
-						input[l + 1].Substring("  Starting items: ".Length).Split(", ").Select(i => long.Parse(i)).ToList(),
-						input[l + 2].Substring("  Operation: new = ".Length),
-						int.Parse(input[l + 3].Substring("  Test: divisible by ".Length)),
+						input[l + 1]["  Starting items: ".Length..].Split(", ").Select(i => long.Parse(i)).ToList(),
+						input[l + 2]["  Operation: new = ".Length..],
+						int.Parse(input[l + 3]["  Test: divisible by ".Length..]),
 						new int[]
 						{
-							int.Parse(input[l + 4].Substring("    If true: throw to monkey ".Length)),
-							int.Parse(input[l + 5].Substring("    If false: throw to monkey ".Length))
-						},
-						0
+							int.Parse(input[l + 4]["    If true: throw to monkey ".Length..]),
+							int.Parse(input[l + 5]["    If false: throw to monkey ".Length..])
+						}
 					));
 			}
 
@@ -70,7 +69,7 @@ namespace Ujeby.AoC.App.Year2022.Day11
 
 		private static long GetItemLevel(string op, long itemLevel)
 		{
-			var value = op.Substring("old * ".Length);
+			var value = op["old * ".Length..];
 			if (op.Contains(" * ")) 
 			{
 				if (value == "old")
@@ -79,12 +78,7 @@ namespace Ujeby.AoC.App.Year2022.Day11
 					return itemLevel * int.Parse(value);
 			}
 			else if (op.Contains(" + "))
-			{
-				if (value == "old")
-					return itemLevel + itemLevel;
-				else
-					return itemLevel + int.Parse(value);
-			}
+				return itemLevel + int.Parse(value);
 
 			else if (op.Contains(" / "))
 				return itemLevel / long.Parse(value);
