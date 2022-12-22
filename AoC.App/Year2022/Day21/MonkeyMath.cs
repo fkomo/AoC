1dfa4972c3e4f1bf55a46a9e6739757af50aba70
filip komorovsky <filip.komorovsky@gmail.com>
2022-12-22 18:40:24 +0100

    2022/21 part2

--- AoC.App/Year2022/Day21/MonkeyMath.cs
+++ AoC.App/Year2022/Day21/MonkeyMath.cs
@@ -18,19 +18,76 @@ namespace Ujeby.AoC.App.Year2022.Day21
 				});
 
 			// part1
-			long? answer1 = MonkeyYellSimple("root", monkeys);
+			long? answer1 = MonkeyYell("root", monkeys);
 
 			// part2
-			long? answer2 = null;
-			long human = 301; // sample
-			// TODO 2022/21 part2
-			if (CheckHuman(human, monkeys))
-				answer2 = human;
+			var humanSide = ContainsName((string)monkeys["root"][0], "humn", monkeys) ? 0 : 2;
+			long? answer2 = MonkeyYell((string)monkeys["root"][(humanSide + 2) % 4], monkeys, human: 0);
+
+			var op = (string)monkeys["root"][humanSide];
+			var pathToHuman = PathTo((string)monkeys["root"][humanSide], "humn", monkeys).Reverse().ToArray();
+			foreach (var path in pathToHuman)
+			{
+				if (path == 0)
+				{
+					var other = (string)monkeys[op][2];
+					switch ((string)monkeys[op][1])
+					{
+						case "+": answer2 = answer2 - MonkeyYell(other, monkeys); break;
+						case "-": answer2 = answer2 + MonkeyYell(other, monkeys); break;
+						case "*": answer2 = answer2 / MonkeyYell(other, monkeys); break;
+						case "/": answer2 = answer2 * MonkeyYell(other, monkeys); break;
+						default:
+							break;
+					}
+				}
+				else
+				{
+					var other = (string)monkeys[op][0];
+					switch ((string)monkeys[op][1])
+					{
+						case "+": answer2 = answer2 - MonkeyYell(other, monkeys); break;
+						case "-": answer2 = MonkeyYell(other, monkeys) - answer2; break;
+						case "*": answer2 = answer2 / MonkeyYell(other, monkeys); break;
+						case "/": answer2 = MonkeyYell(other, monkeys) / answer2; break;
+						default:
+							break;
+					}
+				}
+
+				op = (string)monkeys[op][path];
+			}
+
+			// verification
+			//if (!CheckHumanYell(answer2.Value, monkeys))
+			//	answer2 = null;
 
 			return (answer1?.ToString(), answer2?.ToString());
 		}
 
-		private static long MonkeyYellSimple(string name, Dictionary<string, object[]> monkeys,
+		private int[] PathTo(string monkey, string name, Dictionary<string, object[]> monkeys)
+		{
+			if (monkeys[monkey].Length == 3)
+			{
+				if ((string)monkeys[monkey][0] == name)
+					return new int[] { 0 };
+
+				else if ((string)monkeys[monkey][2] == name)
+					return new int[] { 2 };              
+					
+				var left = PathTo((string)monkeys[monkey][0], name, monkeys);
+				if (left.Any())
+					return left.Concat(new int[] { 0 }).ToArray();
+
+				var right = PathTo((string)monkeys[monkey][2], name, monkeys);
+				if (right.Any())
+					return right.Concat(new int[] { 2 }).ToArray();
+			}
+
+			return Array.Empty<int>();
+		}
+
+		private static long MonkeyYell(string name, Dictionary<string, object[]> monkeys,
 			long? human = null)
 		{
 			if (name == "humn" && human.HasValue)
@@ -40,8 +97,8 @@ namespace Ujeby.AoC.App.Year2022.Day21
 			if (yell.Length == 1)
 				return (long)yell[0];
 
-			var left = MonkeyYellSimple((string)yell[0], monkeys, human: human);
-			var right = MonkeyYellSimple((string)yell[2], monkeys, human: human);
+			var left = MonkeyYell((string)yell[0], monkeys, human: human);
+			var right = MonkeyYell((string)yell[2], monkeys, human: human);
 
 			switch ((string)yell[1])
 			{
@@ -56,10 +113,21 @@ namespace Ujeby.AoC.App.Year2022.Day21
 			throw new NotImplementedException();
 		}
 
-		private static bool CheckHuman(long human, Dictionary<string, object[]> monkeys)
+		private static bool ContainsName(string monkey, string name, Dictionary<string, object[]> monkeys)
 		{
-			var left = MonkeyYellSimple((string)monkeys["root"][0], monkeys, human: human);
-			var right = MonkeyYellSimple((string)monkeys["root"][2], monkeys, human: human);
+			if (monkey == name)
+				return true;
+
+			if (monkeys[monkey].Length == 1)
+				return false;
+
+			return ContainsName((string)monkeys[monkey][0], name, monkeys) || ContainsName((string)monkeys[monkey][2], name, monkeys);
+		}
+
+		private static bool CheckHumanYell(long human, Dictionary<string, object[]> monkeys)
+		{
+			var left = MonkeyYell((string)monkeys["root"][0], monkeys, human: human);
+			var right = MonkeyYell((string)monkeys["root"][2], monkeys, human: human);
 
 			return left == right;
 		}
