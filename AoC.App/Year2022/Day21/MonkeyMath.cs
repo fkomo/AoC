bdef023af273d1193fa20f6da4004b4bd45c2de9
filip komorovsky <filip.komorovsky@gmail.com>
2022-12-22 14:23:18 +0100

    2022/21 part2 sample

--- AoC.App/Year2022/Day21/MonkeyMath.cs
+++ AoC.App/Year2022/Day21/MonkeyMath.cs
@@ -7,37 +7,60 @@ namespace Ujeby.AoC.App.Year2022.Day21
 		protected override (string, string) SolveProblem(string[] input)
 		{
 			var monkeys = input.ToDictionary(
-				line => line[..line.IndexOf(':')], 
-				line => line[(line.IndexOf(": ") + ": ".Length)..]);
+				line => line[..line.IndexOf(':')],
+				line =>
+				{
+					var split = line[(line.IndexOf(": ") + ": ".Length)..].Split(' ');
+					if (split.Length == 1)
+						return new object[] { long.Parse(split[0]) };
+
+					return new object[] { split[0], split[1], split[2] };
+				});
 
 			// part1
-			long? answer1 = MonkeyYell("root", monkeys);
+			long? answer1 = MonkeyYellSimple("root", monkeys);
 
 			// part2
 			long? answer2 = null;
+			long human = 301; // sample
+			if (CheckHuman(human, monkeys))
+				answer2 = human;
 
 			return (answer1?.ToString(), answer2?.ToString());
 		}
 
-		private static long MonkeyYell(string name, Dictionary<string, string> monkeys)
+		private static long MonkeyYellSimple(string name, Dictionary<string, object[]> monkeys,
+			long? human = null)
 		{
+			if (name == "humn" && human.HasValue)
+				return human.Value;
+
 			var yell = monkeys[name];
-			if (long.TryParse(yell, out long result))
-				return result;
+			if (yell.Length == 1)
+				return (long)yell[0];
 
-			var split = yell.Split(' ');
-			var left = MonkeyYell(split[0], monkeys);
-			var right = MonkeyYell(split[2], monkeys);
+			var left = MonkeyYellSimple((string)yell[0], monkeys, human: human);
+			var right = MonkeyYellSimple((string)yell[2], monkeys, human: human);
 
-			switch (split[1])
+			switch ((string)yell[1])
 			{
 				case "+": return left + right;
 				case "-": return left - right;
 				case "*": return left * right;
 				case "/": return left / right;
+				default:
+					break;
 			}
 
 			throw new NotImplementedException();
+		}
+
+		private static bool CheckHuman(long human, Dictionary<string, object[]> monkeys)
+		{
+			var left = MonkeyYellSimple((string)monkeys["root"][0], monkeys, human: human);
+			var right = MonkeyYellSimple((string)monkeys["root"][2], monkeys, human: human);
+
+			return left == right;
 		}
 	}
 }
