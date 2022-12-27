479b045b02896e487be88633673a41e02ce433b0
fkomo <filip.komorovsky@gmail.com>
2022-12-27 22:05:26 +0100

    2022/24

--- AoC.App/Year2022/Day24/BlizzardBasin.cs
+++ AoC.App/Year2022/Day24/BlizzardBasin.cs
@@ -1,9 +1,18 @@
 using Ujeby.AoC.Common;
+using Ujeby.Vectors;
 
 namespace Ujeby.AoC.App.Year2022.Day24
 {
 	public class BlizzardBasin : PuzzleBase
 	{
+		private static readonly Dictionary<char, v2i> _windDir = new()
+		{
+			{ '>', new(1, 0) },
+			{ 'v', new(0, 1) },
+			{ '<', new(-1, 0) },
+			{ '^', new(0, -1) }
+		};
+
 		protected override (string, string) SolveProblem(string[] input)
 		{
 			// TODO 2022/24
