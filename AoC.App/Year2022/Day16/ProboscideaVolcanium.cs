4012398dc929af9c00583dd6fefcdd2fc3e48b6a
filip komorovsky <filip.komorovsky@gmail.com>
2023-01-07 17:14:13 +0100

    2022/16 optimizations

--- AoC.App/Year2022/Day16/ProboscideaVolcanium.cs
+++ AoC.App/Year2022/Day16/ProboscideaVolcanium.cs
@@ -39,28 +39,22 @@ namespace Ujeby.AoC.App.Year2022.Day16
 			dist = Alg.FloydWarshall.ShortestPath(dist);
 
 			Debug.Line($"all valves ({valves.Count}): {string.Join(", ", valves.Keys)}");
-
 			foreach (var toRemove in valves.Where(v => v.Key != "AA" && v.Value.FlowRate == 0).Select(v => v.Key))
 				valves.Remove(toRemove);
-
 			Debug.Line($"nonZero valves ({valves.Count}): {string.Join(", ", valves.Keys)}");
 
 			// part1
-			long? answer1 = MoveToValve(valves, dist, "AA", Array.Empty<string>(), Array.Empty<string>(), out string[] path);
-			Debug.Line($"part1 path: {string.Join(", ", path)}");
+			var aaIdx = valves["AA"].Idx;
+			long? answer1 = MoveToValve(valves, dist, Array.Empty<int>(), Array.Empty<int>(),
+				valveIdx: aaIdx);
 
 			// part2
-			// TODO 2022/16 p2 OPTIMIZE (12s)
+			// TODO 2022/16 p2 OPTIMIZE (2s)
 			//long? answer2 = long.MinValue;
+			//var noStartIdx = valves.Where(v => v.Key != "AA").Select(vk => vk.Value.Idx).ToArray();
+			//var halfLength = noStartIdx.Length / 2;
 
-			//var noStartValves = valves.Where(v => v.Key != "AA").Select(vk => vk.Key).ToArray();
-
-			//var answer2Path1 = Array.Empty<string>();
-			//var answer2Path2 = Array.Empty<string>();
-
-			//var halfLength = noStartValves.Length / 2;
-
-			//var kComb = Combinatorics.KCombinations(noStartValves, halfLength).ToArray();
+			//var kComb = Combinatorics.KCombinations(noStartIdx, halfLength).ToArray();
 			//// only half of combinations is needed (the other will be mirrored)
 			//kComb = kComb.Take(kComb.Length / 2).ToArray();
 
@@ -68,23 +62,17 @@ namespace Ujeby.AoC.App.Year2022.Day16
 
 			//foreach (var valves1 in kComb)
 			//{
-			//	var p1 = MoveToValve(valves, dist, "AA",
-			//		Array.Empty<string>(), valves1.ToArray(), out string[] path1,
+			//	var p1 = MoveToValve(valves, dist, Array.Empty<int>(), valves1.ToArray(),
+			//		valveIdx: aaIdx,
 			//		minutesLeft: 26);
 
-			//	var p2 = MoveToValve(valves, dist, "AA",
-			//		Array.Empty<string>(), noStartValves.Except(valves1).ToArray(), out string[] path2,
+			//	var p2 = MoveToValve(valves, dist, Array.Empty<int>(), noStartIdx.Except(valves1).ToArray(),
+			//		valveIdx: aaIdx,
 			//		minutesLeft: 26);
 
 			//	if ((p1 + p2) > answer2)
-			//	{
 			//		answer2 = p1 + p2;
-			//		answer2Path1 = path1;
-			//		answer2Path2 = path2;
-			//	}
 			//}
-			//Debug.Line($"part2 path1 (you):      {string.Join(", ", answer2Path1)}");
-			//Debug.Line($"part2 path2 (elephant): {string.Join(", ", answer2Path2)}");
 			long? answer2 = 2723;
 
 			Debug.Line();
@@ -122,47 +110,29 @@ namespace Ujeby.AoC.App.Year2022.Day16
 			return valves;
 		}
 
-		private static int MoveToValve(Dictionary<string, Valve> valves, int[,] dist,
-			string valveName, string[] opened, string[] ignore, out string[] path,
-			int openedFlowRate = 0, int pressureReleased = 0, int minutesLeft = 30)
+		private static int MoveToValve(Dictionary<string, Valve> valves, int[,] dist, int[] openedIdx, int[] ignoreIdx,
+			int valveIdx = 0, int valveFlowRate = 0, int openedFlowRate = 0, int pressureReleased = 0, int minutesLeft = 30)
 		{
-			var valve = valves[valveName];
-
 			var bestPressure = int.MinValue;
-			var bestPath = Array.Empty<string>();
-
 			foreach (var nextValve in valves.Values)
 			{
-				var d = dist[valve.Idx, nextValve.Idx] + 1;
+				var d = dist[valveIdx, nextValve.Idx] + 1;
 
-				if (nextValve.Idx == valve.Idx || opened.Contains(nextValve.Name) || d >= minutesLeft || ignore.Contains(nextValve.Name))
+				if (nextValve.Idx == valveIdx || openedIdx.Contains(nextValve.Idx) || d >= minutesLeft || ignoreIdx.Contains(nextValve.Idx))
 					continue;
 
-				var p = MoveToValve(valves, dist, nextValve.Name,
-					(valveName != "AA") ? opened.Concat(new[] { valveName }).ToArray() : opened,
-					ignore,
-					out path,
-					openedFlowRate: openedFlowRate + valve.FlowRate,
-					pressureReleased: pressureReleased + d * (valve.FlowRate + openedFlowRate),
+				var p = MoveToValve(valves, dist, openedIdx.Concat(new[] { valveIdx }).ToArray(), ignoreIdx,
+					valveIdx: nextValve.Idx,
+					valveFlowRate: nextValve.FlowRate,
+					openedFlowRate: openedFlowRate + valveFlowRate,
+					pressureReleased: pressureReleased + d * (valveFlowRate + openedFlowRate),
 					minutesLeft: minutesLeft - d);
 
 				if (p > bestPressure)
-				{
 					bestPressure = p;
-					bestPath = path.ToArray();
-				}
 			}
 
-			// all valves are open
-			if (bestPressure == int.MinValue)
-			{
-				path = (valveName != "AA") ? opened.Concat(new[] { valveName }).ToArray() : opened;
-				bestPressure = pressureReleased + minutesLeft * (openedFlowRate + valve.FlowRate);
-			}
-			else
-				path = bestPath.ToArray();
-
-			return bestPressure;
+			return (bestPressure == int.MinValue) ? pressureReleased + minutesLeft * (openedFlowRate + valveFlowRate) : bestPressure;
 		}
 	}
 }
