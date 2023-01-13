39abf46de638a2e837df0fff3b9ab7bef122fadd
fkomo <filip.komorovsky@gmail.com>
2023-01-13 21:42:33 +0100

    2020/03

--- AoC.App/Program.cs
+++ AoC.App/Program.cs
@@ -76,7 +76,7 @@ namespace Ujeby.AoC.App
 				{
 					new Year2020.Day01.ReportRepair()               { Answer = new string[] { "927684", "292093004" } },
 					new Year2020.Day02.PasswordPhilosophy()         { Answer = new string[] { "474", "745" } },
-					new Year2020.Day03.Puzzle202003()               { Answer = new string[] { null, null } },
+					new Year2020.Day03.TobogganTrajectory()			{ Answer = new string[] { "223", "3517401300" } },
 					new Year2020.Day04.Puzzle202004()               { Answer = new string[] { null, null } },
 					new Year2020.Day05.Puzzle202005()               { Answer = new string[] { null, null } },
 					new Year2020.Day06.Puzzle202006()               { Answer = new string[] { null, null } },
