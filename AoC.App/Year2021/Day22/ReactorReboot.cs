f961af942aa80a2ec9c4797111ca7cedb573570d
filip komorovsky <filip.komorovsky@gmail.com>
2023-01-24 16:13:28 +0100

    2021/22 code cleanup

--- AoC.App/Year2021/Day22/ReactorReboot.cs
+++ AoC.App/Year2021/Day22/ReactorReboot.cs
@@ -9,23 +9,21 @@ namespace Ujeby.AoC.App.Year2021.Day22
 	{
 		public struct Cuboid
 		{
-			public bool TurnOn;
-			public AABox3i Area;
-			public long Size() => TurnOn ? (Area.Size + 1).Volume() : -(Area.Size + 1).Volume();
+			public bool State;
+			public AABox3i AABox;
+			public long Size() => State ? (AABox.Size + 1).Volume() : -(AABox.Size + 1).Volume();
 
-			public Cuboid(bool turnOn, AABox3i area)
+			public Cuboid(bool turnOn, AABox3i aab)
 			{
-				TurnOn = turnOn;
-				Area = area;
+				State = turnOn;
+				AABox = aab;
 			}
 
-			public override string ToString() => $"{(TurnOn ? "+" : "")}{Size()} {Area}";
+			public override string ToString() => $"{(State ? "+" : "")}{Size()} {AABox}";
 		}
 
 		protected override (string, string) SolvePuzzle(string[] input)
 		{
-			Debug.Line();
-
 			// part1
 			var target = new AABox3i(new(-50), new(50));
 			var cuboids50 = input.Select(line =>
@@ -33,29 +31,15 @@ namespace Ujeby.AoC.App.Year2021.Day22
 					var n = line.ToNumArray();
 					var area = new AABox3i(new(n[0], n[2], n[4]), new(n[1], n[3], n[5]));
 					if (!target.Intersect(area))
-						return new Cuboid { TurnOn = false, Area = AABox3i.Empty };
+						return new Cuboid { State = false, AABox = AABox3i.Empty };
 
 					return new Cuboid(
 						line.StartsWith("on"),
 						new AABox3i(v3i.Clamp(area.Min, new(-50), new(50)) + 50, v3i.Clamp(area.Max, new(-50), new(50)) + 50));
 				})
-				.Where(c => c.Area != AABox3i.Empty)
+				.Where(c => c.AABox != AABox3i.Empty)
 				.ToArray();
-
-			Debug.Line($"part1: {cuboids50.Length} cuboids in {target}");
-
-			var targetSize = target.Size + 1;
-			var result = new bool[targetSize.Volume()];
-			var offset = new v2i(targetSize.X, targetSize.X * targetSize.Y);
-			foreach (var cuboid in cuboids50)
-			{
-				var c = cuboid.Area.Min;
-				for (c.Z = cuboid.Area.Min.Z; c.Z <= cuboid.Area.Max.Z; c.Z++)
-					for (c.Y = cuboid.Area.Min.Y; c.Y <= cuboid.Area.Max.Y; c.Y++)
-						for (c.X = cuboid.Area.Min.X; c.X <= cuboid.Area.Max.X; c.X++)
-							result[c.Z * offset.Y + c.Y * offset.X + c.X] = cuboid.TurnOn;
-			}
-			long? answer1 = result.Count(c => c);
+			long? answer1 = RebootFinite(target.Size + 1, cuboids50);
 
 			// part2
 			var cuboids = input.Select(line =>
@@ -63,53 +47,64 @@ namespace Ujeby.AoC.App.Year2021.Day22
 				var n = line.ToNumArray();
 				return new Cuboid(line.StartsWith("on"), new AABox3i(new(n[0], n[2], n[4]), new(n[1], n[3], n[5])));
 			}).ToArray();
-			Debug.Line($"part2: {cuboids.Length} cuboids");
-
-			var cList = new List<Cuboid>();
-			long? answer2 = 0;
-			for (var icA = 0; icA < cuboids.Length; icA++)
-			{
-				var cA = cuboids[icA];
-				for (var icB = cList.Count - 1; icB >= 0; icB--)
-				{
-					var cB = cList[icB];
-					if (!cA.Area.Intersect(cB.Area))
-						continue;
-
-					var cAB = AABoxAndAABox(cA.Area, cB.Area);
-					if (cA.TurnOn)
-					{
-						var ccAB = new Cuboid(!cB.TurnOn, cAB);
-						cList.Add(ccAB);
-						answer2 += ccAB.Size();
-					}
-					else
-					{
-						var ccAB = new Cuboid(!cB.TurnOn, cAB);
-						cList.Add(ccAB);
-						answer2 += ccAB.Size();
-					}
-				}
-
-				if (cA.TurnOn)
-				{
-					cList.Add(cA);
-					answer2 += cA.Size();
-				}
-			}
-
-			Debug.Line();
+			long? answer2 = RebootInfinite(cuboids);
 
 			return (answer1?.ToString(), answer2?.ToString());
 		}
 
-		private static AABox3i AABoxAndAABox(AABox3i aab1, AABox3i aab2)
+		private static long RebootFinite(v3i reactorSize, Cuboid[] cuboids)
 		{
-			var x = new long[] { aab1.Min.X, aab1.Max.X, aab2.Min.X, aab2.Max.X }.OrderBy(i => i).ToArray();
-			var y = new long[] { aab1.Min.Y, aab1.Max.Y, aab2.Min.Y, aab2.Max.Y }.OrderBy(i => i).ToArray();
-			var z = new long[] { aab1.Min.Z, aab1.Max.Z, aab2.Min.Z, aab2.Max.Z }.OrderBy(i => i).ToArray();
+			var offset = new v2i(reactorSize.X, reactorSize.X * reactorSize.Y);
 
-			return new AABox3i(new(x[1], y[1], z[1]), new(x[2], y[2], z[2]));
+			var result = new bool[reactorSize.Volume()];
+			foreach (var cuboid in cuboids)
+			{
+				var c = cuboid.AABox.Min;
+				for (c.Z = cuboid.AABox.Min.Z; c.Z <= cuboid.AABox.Max.Z; c.Z++)
+					for (c.Y = cuboid.AABox.Min.Y; c.Y <= cuboid.AABox.Max.Y; c.Y++)
+						for (c.X = cuboid.AABox.Min.X; c.X <= cuboid.AABox.Max.X; c.X++)
+							result[c.Z * offset.Y + c.Y * offset.X + c.X] = cuboid.State;
+			}
+
+			return result.LongCount(c => c);
+		}
+
+		private static long RebootInfinite(Cuboid[] cuboids)
+		{
+			var result = 0L;
+
+			var tmp = new List<Cuboid>();
+			for (var icA = 0; icA < cuboids.Length; icA++)
+			{
+				var cA = cuboids[icA];
+				for (var icB = tmp.Count - 1; icB >= 0; icB--)
+				{
+					var cB = tmp[icB];
+					if (!cA.AABox.And(cB.AABox, out AABox3i cAB))
+						continue;
+
+					if (cA.State)
+					{
+						var ccAB = new Cuboid(!cB.State, cAB);
+						tmp.Add(ccAB);
+						result += ccAB.Size();
+					}
+					else
+					{
+						var ccAB = new Cuboid(!cB.State, cAB);
+						tmp.Add(ccAB);
+						result += ccAB.Size();
+					}
+				}
+
+				if (cA.State)
+				{
+					tmp.Add(cA);
+					result += cA.Size();
+				}
+			}
+
+			return result;
 		}
 	}
 }
