341553de53569c28c080a21b32f6f30f41cfee1f
filip komorovsky <filip.komorovsky@gmail.com>
2023-01-28 21:00:11 +0100

    2021/23 p2

--- AoC.App/Year2021/Day23/Amphipod.cs
+++ AoC.App/Year2021/Day23/Amphipod.cs
@@ -1,3 +1,6 @@
+using OpenTK.Graphics.OpenGL;
+using System.Runtime.Serialization;
+using System.Transactions;
 using Ujeby.AoC.Common;
 using Ujeby.Vectors;
 
@@ -87,6 +90,7 @@ namespace Ujeby.AoC.App.Year2021.Day23
 			Debug.Line();
 
 			// part1
+			//long? answer1 = null;
 			State.Map = new string[]
 			{
 				"#############",
@@ -120,58 +124,57 @@ namespace Ujeby.AoC.App.Year2021.Day23
 
 			_cache.Clear();
 			long? answer1 = Step(start);
-			//long? answer1 = null;
 
 			// part2
-			//var input2 = input.Take(3).Concat(new string[]
-			//{
-			//	"  #D#C#B#A#",
-			//	"  #D#B#A#C#",
-			//}).Concat(input.Skip(3)).ToArray();
-			//State.Map = new string[]
-			//{
-			//	"#############",
-			//	"#           #",
-			//	"### # # # ###",
-			//	"  # # # # #",
-			//	"  # # # # #",
-			//	"  # # # # #",
-			//	"  #########"
-			//};
-			//State.Rooms = new Dictionary<char, v2i[]>
-			//{
-			//	{ 'A', new v2i[] { new(3, 2), new(3, 3), new(3, 4), new(3, 5) } },
-			//	{ 'B', new v2i[] { new(5, 2), new(5, 3), new(5, 4), new(5, 5) } },
-			//	{ 'C', new v2i[] { new(7, 2), new(7, 3), new(7, 4), new(7, 5) } },
-			//	{ 'D', new v2i[] { new(9, 2), new(9, 3), new(9, 4), new(9, 5) } },
-			//};
-			//var start2 = new State
-			//{
-			//	Amphipods = new Amph[]
-			//	{
-			//		new Amph('A', new(3, 2)),
-			//		new Amph('A', new(3, 3)),
-			//		new Amph('A', new(3, 4)),
-			//		new Amph('A', new(3, 5)),
-			//		new Amph('B', new(5, 2)),
-			//		new Amph('B', new(5, 3)),
-			//		new Amph('B', new(5, 4)),
-			//		new Amph('B', new(5, 5)),
-			//		new Amph('C', new(7, 2)),
-			//		new Amph('C', new(7, 3)),
-			//		new Amph('C', new(7, 4)),
-			//		new Amph('C', new(7, 5)),
-			//		new Amph('D', new(9, 2)),
-			//		new Amph('D', new(9, 3)),
-			//		new Amph('D', new(9, 4)),
-			//		new Amph('D', new(9, 5)),
-			//	}.Select(fa => new Amph(input2[fa.Position.Y][(int)fa.Position.X], fa.Position)).ToArray(),
-			//};
-			//start2.Draw();
+			//long? answer2 = null;
+			var input2 = input.Take(3).Concat(new string[]
+			{
+				"  #D#C#B#A#",
+				"  #D#B#A#C#",
+			}).Concat(input.Skip(3)).ToArray();
+			State.Map = new string[]
+			{
+				"#############",
+				"#           #",
+				"### # # # ###",
+				"  # # # # #",
+				"  # # # # #",
+				"  # # # # #",
+				"  #########"
+			};
+			State.Rooms = new Dictionary<char, v2i[]>
+			{
+				{ 'A', new v2i[] { new(3, 2), new(3, 3), new(3, 4), new(3, 5) } },
+				{ 'B', new v2i[] { new(5, 2), new(5, 3), new(5, 4), new(5, 5) } },
+				{ 'C', new v2i[] { new(7, 2), new(7, 3), new(7, 4), new(7, 5) } },
+				{ 'D', new v2i[] { new(9, 2), new(9, 3), new(9, 4), new(9, 5) } },
+			};
+			var start2 = new State
+			{
+				Amphipods = new Amph[]
+				{
+					new Amph('A', new(3, 2)),
+					new Amph('A', new(3, 3)),
+					new Amph('A', new(3, 4)),
+					new Amph('A', new(3, 5)),
+					new Amph('B', new(5, 2)),
+					new Amph('B', new(5, 3)),
+					new Amph('B', new(5, 4)),
+					new Amph('B', new(5, 5)),
+					new Amph('C', new(7, 2)),
+					new Amph('C', new(7, 3)),
+					new Amph('C', new(7, 4)),
+					new Amph('C', new(7, 5)),
+					new Amph('D', new(9, 2)),
+					new Amph('D', new(9, 3)),
+					new Amph('D', new(9, 4)),
+					new Amph('D', new(9, 5)),
+				}.Select(fa => new Amph(input2[fa.Position.Y][(int)fa.Position.X], fa.Position)).ToArray(),
+			};
+			start2.Draw();
 
-			//_cache.Clear();
-			//long? answer2 = Step(start2);
-			long? answer2 = null;
+			_cache.Clear();
+			long? answer2 = Step(start2);
 
 			return (answer1?.ToString(), answer2?.ToString());
 		}
@@ -180,81 +183,109 @@ namespace Ujeby.AoC.App.Year2021.Day23
 
 		public static long Step(State state)
 		{
-			//state.Draw();
-
 			var cacheKey = state.ToString();
 			if (_cache.ContainsKey(cacheKey))
 				return _cache[cacheKey];
 
-			var amphipodsToMove = new List<int>();
-
-			// first move amphs from hallway to correct rooms
+			var amphipodsInHallway = new List<int>();
 			for (var ia = 0; ia < state.Amphipods.Length; ia++)
 			{
 				var amph = state.Amphipods[ia];
 				var amphRooms = State.Rooms[amph.Type];
 
-				// if amphipode is in correct room
-				if (amph.Position.Y > 1 && amph.Position.X == amphRooms[0].X)
-				{
-					//if (amph.Position == amphRooms[1])
-					//	continue;
-
-					var ir = (int)amph.Position.Y - 2;
-					if (Enumerable.Range(ir + 1, amphRooms.Length - ir - 1)
-						.All(i => state.OccupiedBy(amphRooms[i]) == amph.Type))
-						continue;
-				}
-
-				// if room is completed
+				// all amphs of this type are in correct room
 				if (state.RoomComplete(amph.Type))
 					continue;
 
-				// if amphipode cant move in any direction
-				if (v2i.RightDownLeftUp.All(d => !state.Empty(amph.Position + d)))
-					continue;
-
-				// amphipode in hallway
-				if (!State.Hallway.Contains(amph.Position))
-				{
-					amphipodsToMove.Add(ia);
-					continue;
-				}
-
-				var p = amph.Position;
-				var rx = amphRooms[0].X;
-				var dir = new v2i(amph.Position.X > rx ? -1 : 1, 0);
-				for (p += dir; p.X != rx && state.Empty(p); p += dir)
-				{
-				}
-
-				// if path to room is blocked
-				if (p.X != rx)
-					continue;
-
-				for (var ir = amphRooms.Length - 1; ir >= 0; ir--)
-				{
-					// if correct destination room is empty
-					if (state.OccupiedBy(amphRooms[ir]) == 0 &&
-						Enumerable.Range(ir + 1, amphRooms.Length - ir - 1).All(i => state.OccupiedBy(amphRooms[i]) == amph.Type))
-					{
-						state.Move(ia, amphRooms[ir]);
-						//state.Draw();
-						break;
-					}
-				}
+				// amph in hallway
+				if (State.Hallway.Contains(amph.Position))
+					amphipodsInHallway.Add(ia);
 			}
 
-			cacheKey = state.ToString();
-			if (state.Complete())
+			// move all amphs from hallway to correct rooms
+			var movementInHallway = true;
+			while (movementInHallway)
 			{
-				_cache[cacheKey] = state.EnergyUsed; 
-				return state.EnergyUsed;
+				movementInHallway = false;
+				foreach (var ia in amphipodsInHallway)
+				{
+					var amph = state.Amphipods[ia];
+					var amphRooms = State.Rooms[amph.Type];
+
+					// if amphipode cant move in any direction
+					if (v2i.RightDownLeftUp.All(d => !state.Empty(amph.Position + d)))
+						continue;
+
+					var p = amph.Position;
+					var rx = amphRooms[0].X;
+					var dir = new v2i(amph.Position.X > rx ? -1 : 1, 0);
+					for (p += dir; p.X != rx && state.Empty(p); p += dir)
+					{
+					}
+
+					// if path to room is blocked
+					if (p.X != rx)
+						continue;
+
+					for (var ir = amphRooms.Length - 1; ir >= 0; ir--)
+					{
+						// if correct destination room is empty
+						if (state.OccupiedBy(amphRooms[ir]) == 0 &&
+							Enumerable.Range(ir + 1, amphRooms.Length - ir - 1).All(i => state.OccupiedBy(amphRooms[i]) == amph.Type))
+						{
+							state.Move(ia, amphRooms[ir]);
+							//state.Draw();
+
+							amphipodsInHallway.Remove(ia);
+							movementInHallway = true;
+							break;
+						}
+					}
+
+					if (movementInHallway)
+						break;
+				}
 			}
 
-			// go through all remaining possibilities
+			if (state.Complete())
+				return state.EnergyUsed;
+
+			var amphipodsInWrongRooms = new List<int>();
+			for (var ia = 0; ia < state.Amphipods.Length; ia++)
+			{
+				var amph = state.Amphipods[ia];
+				var amphRooms = State.Rooms[amph.Type];
+
+				// all amphs of this type are in correct room
+				if (state.RoomComplete(amph.Type))
+					continue;
+
+				// amph in hallway
+				if (State.Hallway.Contains(amph.Position))
+					continue;
+
+				// amph in room
+
+				// amph cannot move
+				else if (v2i.RightDownLeftUp.All(d => !state.Empty(amph.Position + d)))
+					continue;
+
+				// amph can move
+
+				// amph in wrong room, or in correct room but blocking somebody else who shouldnt be there
+				else if (!amphRooms.Contains(amph.Position) ||
+					Enumerable.Range((int)amph.Position.Y - 2 + 1, amphRooms.Length - ((int)amph.Position.Y - 2) - 1)
+						.Any(ry => state.OccupiedBy(new v2i(amph.Position.X, ry + 2)) != amph.Type))
+					amphipodsInWrongRooms.Add(ia);
+
+				// amph in correct room, no movement needed
+			}
+
+			//state.Draw();
+
+			// move all amphs in wrong rooms
 			var leastEnergyUsed = long.MaxValue;
-			foreach (var ia in amphipodsToMove)
+			foreach (var ia in amphipodsInWrongRooms)
 			{
 				var apx = state.Amphipods[ia].Position.X;
 
@@ -268,7 +299,7 @@ namespace Ujeby.AoC.App.Year2021.Day23
 					leastEnergyUsed = MoveToHallway(state, ia, hall, leastEnergyUsed);
 			}
 
-			_cache[cacheKey] = leastEnergyUsed;
+			_cache[state.ToString()] = leastEnergyUsed;
 			return leastEnergyUsed;
 		}
 
