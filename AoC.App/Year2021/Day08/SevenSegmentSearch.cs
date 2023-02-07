915a049b467f5b39eb168e0df84a6e2a33f507bd
filip komorovsky <filip.komorovsky@gmail.com>
2023-02-07 16:17:19 +0100

    2021/25 p1

--- AoC.App/Year2021/Day08/SevenSegmentSearch.cs
+++ AoC.App/Year2021/Day08/SevenSegmentSearch.cs
@@ -7,14 +7,14 @@ namespace Ujeby.AoC.App.Year2021.Day08
 		protected override (string, string) SolvePuzzle(string[] input)
 		{
 			// part1
-			long answer1 = input
+			long? answer1 = input
 				.Sum(i => i.Split(" | ")[1].Split(' ').Count(s => s.Length == 2 || s.Length == 3 || s.Length == 4 || s.Length == 7));
 
 			// part2
 			// TODO 2021/08 p2
 			long? answer2 = null;
 
-			return (answer1.ToString(), answer2?.ToString());
+			return (answer1?.ToString(), answer2?.ToString());
 		}
 	}
 }
