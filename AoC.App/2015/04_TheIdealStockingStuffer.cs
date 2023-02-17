0c78f7fe233e73612a53aa1a077f51d096059945
filip komorovsky <filip.komorovsky@gmail.com>
2023-02-17 15:56:43 +0100

    2015/04

--- AoC.App/2015/04_TheIdealStockingStuffer.cs
+++ AoC.App/2015/04_TheIdealStockingStuffer.cs
@@ -1,21 +1,46 @@
+using System.Security.Cryptography;
+using System.Text;
 using Ujeby.AoC.Common;
 
 namespace Ujeby.AoC.App._2015_04
 {
-	[AoCPuzzle(Year = 2015, Day = 04, Answer1 = null, Answer2 = null)]
+	[AoCPuzzle(Year = 2015, Day = 04, Answer1 = "282749", Answer2 = "9962624")]
 	public class TheIdealStockingStuffer : PuzzleBase
 	{
+		private readonly static MD5 _md5 = MD5.Create();
+
 		protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 		{
-			string answer1 = null, answer2 = null;
-
-			// TODO 2015/04
+			var secretKey = input.Single();
 
 			// part1
+			long answer1 = 1;
+			while (true)
+			{
+				var value = $"{secretKey}{answer1}";
+				if (CheckHash(value))
+					break;
+
+				answer1++;
+			}
 
 			// part2
+			long answer2 = answer1 + 1;
+			while (true)
+			{
+				var value = $"{secretKey}{answer2}";
+				if (CheckHash(value, zeroes: 6))
+					break;
 
-			return (answer1, answer2);
+				answer2++;
+			}
+
+			return (answer1.ToString(), answer2.ToString());
 		}
+
+		private static bool CheckHash(string value,
+			int zeroes = 5)
+			=> Convert.ToHexString(_md5.ComputeHash(Encoding.ASCII.GetBytes(value)))
+				.Take(zeroes).All(c => c == '0');
 	}
 }
