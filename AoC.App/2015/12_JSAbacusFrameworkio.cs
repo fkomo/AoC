2922d1bac2918dcd491fbe65400bbd47a18c3ea1
filip komorovsky <filip.komorovsky@gmail.com>
2023-02-21 22:44:26 +0100

    2015/12

--- AoC.App/2015/12_JSAbacusFrameworkio.cs
+++ AoC.App/2015/12_JSAbacusFrameworkio.cs
@@ -5,7 +5,7 @@ using Ujeby.Tools.StringExtensions;
 
 namespace Ujeby.AoC.App._2015_12
 {
-	[AoCPuzzle(Year = 2015, Day = 12, Answer1 = "111754", Answer2 = null)]
+	[AoCPuzzle(Year = 2015, Day = 12, Answer1 = "111754", Answer2 = "65402")]
 	public class JSAbacusFrameworkio : PuzzleBase
 	{
 		protected override (string Part1, string Part2) SolvePuzzle(string[] input)
@@ -22,132 +22,63 @@ namespace Ujeby.AoC.App._2015_12
 		private static long SumJsonWithoutRed(string json)
 		{
 			var sum = 0L;
-			// object
-			if (json[0] == '{')
-			{
-				json = json[1..^1];
-				for (var i = 0; i < json.Length; i++)
-				{
-					if (json[i] == ',')
-						continue;
+			var isArray = json[0] != '{';
 
+			json = json[1..^1];
+			for (var i = 0; i < json.Length; i++)
+			{
+				if (json[i] == ',')
+					continue;
+
+				int sIdx = i, eIdx;
+				if (!isArray)
+				{
+					// object property name
 					var propertyName = json[(i + 1)..json.IndexOf('\"', i + 1)];
+					sIdx += propertyName.Length + 3; // 2x double qotes "" + :
 
-					var sIdx = i + propertyName.Length + 3; // 2x double qotes "" + :
-					var eIdx = sIdx;
-					if (json[sIdx] == '{' || json[sIdx] == '[')
-					{
-						eIdx = IndexOfClosingBracket(json, sIdx);
-						sum += SumJsonWithoutRed(json[sIdx..eIdx]);
-					}
-					else
-					{
-						// string
-						if (json[sIdx] == '\"')
-						{
-							eIdx = json.IndexOf('\"', sIdx + 1);
-							var value = json[(sIdx+1)..eIdx];
-						}
-						// number
-						else
-						{
-							var n = string.Empty;
-							if (json[sIdx] == '-')
-							{
-								n = "-";
-								sIdx++;
-							}
-
-							for (eIdx = sIdx; eIdx < json.Length && char.IsDigit(json[eIdx]); eIdx++)
-								n += json[eIdx];
-							if (!int.TryParse(n, out int value))
-								throw new Exception($"'{json}' -> {sIdx}..{eIdx}={n}");
-
-							sum += value;	
-						}
-					}
-					i = eIdx;
+					if (propertyName == "red" && !isArray)
+						return 0;
 				}
-			}
-			// array
-			else if (json[0] == '[')
-			{
-				json = json[1..^1];
-				for (var i = 0; i < json.Length; i++)
+
+				// nested object/array				
+				if (json[sIdx] == '{' || json[sIdx] == '[')
 				{
-					if (json[i] == ',')
-						continue;
-
-					var sIdx = i;
-					var eIdx = sIdx;
-					if (json[sIdx] == '{' || json[sIdx] == '[')
+					eIdx = json.IndexOfClosingBracket(sIdx);
+					sum += SumJsonWithoutRed(json[sIdx..(eIdx + 1)]);
+				}
+				// value
+				else
+				{
+					// string
+					if (json[sIdx] == '\"')
 					{
-						eIdx = IndexOfClosingBracket(json, sIdx);
-						sum += SumJsonWithoutRed(json[sIdx..eIdx]);
+						eIdx = json.IndexOf('\"', sIdx + 1);
+						var value = json[(sIdx+1)..eIdx];
+
+						if (value == "red" && !isArray)
+							return 0;
 					}
+					// number
 					else
 					{
-						// string
-						if (json[sIdx] == '\"')
+						var n = string.Empty;
+						if (json[sIdx] == '-')
 						{
-							eIdx = json.IndexOf('\"', sIdx + 1);
-							var value = json[(sIdx + 1)..eIdx];
+							n = "-";
+							sIdx++;
 						}
-						// number
-						else
-						{
-							var n = string.Empty;
-							if (json[sIdx] == '-')
-							{
-								n = "-";
-								sIdx++;
-							}
 
-							for (eIdx = sIdx; eIdx < json.Length && char.IsDigit(json[eIdx]); eIdx++)
-								n += json[eIdx];
-							if (!int.TryParse(n, out int value))
-								throw new Exception($"'{json}' -> {sIdx}..{eIdx}={n}");
+						for (eIdx = sIdx; eIdx < json.Length && char.IsDigit(json[eIdx]); eIdx++)
+							n += json[eIdx];
 
-							sum += value;
-						}
+						sum += int.Parse(n);	
 					}
-					i = eIdx;
 				}
-
+				i = eIdx;
 			}
 
 			return sum;
-		}
-
-		/// <summary>
-		/// returns index of corresponding closing bracket (with nesting in mind)
-		/// </summary>
-		/// <param name="s"></param>
-		/// <param name="openingBracketIndex"></param>
-		/// <param name="brackets"></param>
-		/// <returns></returns>
-		public static int IndexOfClosingBracket(string s, 
-			int openingBracketIndex = 0, string openingBrackets = "{([", string closingBrackets = "})]")
-		{
-			var i = openingBracketIndex;
-			var nest = new int[openingBrackets.Length];
-			do
-			{
-				var nOpenId = openingBrackets.IndexOf(s[i]);
-				if (nOpenId != -1)
-					nest[nOpenId]++;
-				else
-				{
-					var nCloseId = closingBrackets.IndexOf(s[i]);
-					if (nCloseId != -1)
-						nest[nCloseId]--;
-				}
-
-				i++;
-			}
-			while (nest.Any(n => n > 0));
-
-			return i;
 		}
 	}
 }
