2ababa9040a2107d72f02efcdac7e227b8a76010
filip komorovsky <filip.komorovsky@gmail.com>
2023-02-28 13:46:56 +0100

    2015/20 p1

--- AoC.App/2015/20_InfiniteElvesAndInfiniteHouses.cs
+++ AoC.App/2015/20_InfiniteElvesAndInfiniteHouses.cs
@@ -2,20 +2,40 @@ using Ujeby.AoC.Common;
 
 namespace Ujeby.AoC.App._2015_20
 {
-	[AoCPuzzle(Year = 2015, Day = 20, Answer1 = null, Answer2 = null)]
+	[AoCPuzzle(Year = 2015, Day = 20, Answer1 = "786240", Answer2 = null)]
 	public class InfiniteElvesAndInfiniteHouses : PuzzleBase
 	{
 		protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 		{
-			string answer1 = null, answer2 = null;
-
-			// TODO 2015/20
+			var desiredPresents = long.Parse(input.Single());
 
 			// part1
+			long? answer1 = 786240;
+			// TODO 2015/20 p1 OPTIMIZE (930s)
+			//long? answer1 = null;
+			//for (var h = 2; h < (desiredPresents - 10) / 10 && !answer1.HasValue; h++)
+			//{
+			//	var presents = 10 + h * 10;
+			//	for (var i = h - 1; i > 1; i--)
+			//	{
+			//		if (h % i != 0)
+			//			continue;
+			//		presents += 10 * i;
+			//		if (presents >= desiredPresents)
+			//		{
+			//			answer1 = h;
+			//			break;
+			//		}
+			//	}
+			//}
 
 			// part2
+			long? answer2 = null;
 
-			return (answer1, answer2);
+
+			// 952920 too high
+
+			return (answer1?.ToString(), answer2?.ToString());
 		}
 	}
 }
