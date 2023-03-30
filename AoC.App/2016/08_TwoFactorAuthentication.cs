e8a1aece2cd54aa35a3f48762d425db0636b95d6
filip komorovsky <filip.komorovsky@gmail.com>
2023-03-30 13:48:56 +0200

    2016/08

--- AoC.App/2016/08_TwoFactorAuthentication.cs
+++ AoC.App/2016/08_TwoFactorAuthentication.cs
@@ -1,20 +1,82 @@
 using Ujeby.AoC.Common;
+using Ujeby.Tools.StringExtensions;
+using Ujeby.Vectors;
 
 namespace Ujeby.AoC.App._2016_08;
 
-[AoCPuzzle(Year = 2016, Day = 08, Answer1 = null, Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2016, Day = 08, Answer1 = "119", Answer2 = "ZFHFSFOGPO", Skip = false)]
 public class TwoFactorAuthentication : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		// TODO 2016/08
-
 		// part1
-		string answer1 = null;
+		var screenSize = new v2i(50, 6);
+		var screen = new HashSet<v2i>();
+		foreach (var op in input)
+		{
+			var param = new v2i(op.ToNumArray());
+			if (op.StartsWith("rect"))
+			{
+				for (var y = 0; y < param.Y; y++)
+					for (var x = 0; x < param.X; x++)
+						screen.Add(new(x, y));
+			}
+			else if (op.StartsWith("rotate column"))
+			{
+				var tmp = new HashSet<v2i>();
+				for (var y = screenSize.Y - 1; y >= 0; y--)
+				{
+					var old = new v2i(param.X, y);
+					if (!screen.Contains(old))
+						continue;
+
+					screen.Remove(old);
+					tmp.Add(new v2i(param.X, (y + param.Y) % screenSize.Y));
+				}
+
+				foreach (var p in tmp)
+					screen.Add(p);
+			}
+			else if (op.StartsWith("rotate row"))
+			{
+				var tmp = new HashSet<v2i>();
+				for (var x = screenSize.X - 1; x >= 0; x--)
+				{
+					var old = new v2i(x, param.X);
+					if (!screen.Contains(old))
+						continue;
+
+					screen.Remove(old);
+					tmp.Add(new v2i((x + param.Y) % screenSize.X, param.X));
+				}
+
+				foreach (var p in tmp)
+					screen.Add(p);
+			}
+
+			//DrawScreen(screen, screenSize);
+		}
+		var answer1 = screen.Count;
 
 		// part2
-		string answer2 = null;
+		var screen2d = new bool[screenSize.Y, screenSize.X];
+		foreach (var p in screen)
+			screen2d[p.Y, p.X] = true;
 
-		return (answer1?.ToString(), answer2?.ToString());
+		var answer2 = CharCodes.ToString(screenSize, screen2d);
+
+		return (answer1.ToString(), answer2?.ToString());
+	}
+
+	private static void DrawScreen(HashSet<v2i> screen, v2i size)
+	{
+		Debug.Line();
+		for (var y = 0; y < size.Y; y++)
+		{
+			var line = string.Empty;
+			for (var x = 0; x < size.X; x++)
+				line += screen.Contains(new v2i(x, y)) ? '#' : '.';
+			Debug.Line(line);
+		}
 	}
 }
