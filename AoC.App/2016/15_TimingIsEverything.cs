124e20ffbe12b0cd9b238d97ba32059c3e40d2e3
filip komorovsky <filip.komorovsky@gmail.com>
2023-05-28 20:37:36 +0200

    2016/15

--- AoC.App/2016/15_TimingIsEverything.cs
+++ AoC.App/2016/15_TimingIsEverything.cs
@@ -1,20 +1,42 @@
 using Ujeby.AoC.Common;
+using Ujeby.Tools.StringExtensions;
+using Ujeby.Vectors;
 
 namespace Ujeby.AoC.App._2016_15;
 
-[AoCPuzzle(Year = 2016, Day = 15, Answer1 = null, Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2016, Day = 15, Answer1 = "122318", Answer2 = "3208583", Skip = false)]
 public class TimingIsEverything : PuzzleBase
 {
+	private const int _positionsCount = 0;
+	private const int _time = 1;
+	private const int _startPosition = 2;
+
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		// TODO 2016/15
+		var discs = input.Select(x => new v3i(x.ToNumArray().Skip(1).ToArray())).ToArray();
+
+		var start = discs[0][_positionsCount] - (discs[0][_startPosition] + 1);
+		var step = discs[0][_positionsCount];
 
 		// part1
-		string answer1 = null;
+		long answer1 = start;
+		while (!CheckOffset(answer1, discs))
+			answer1 += step;
 
 		// part2
-		string answer2 = null;
+		long answer2 = start;
+		while (!CheckOffset(answer2, discs.Concat(new v3i[] { new(11, 0, 0) }).ToArray()))
+			answer2 += step;
 
-		return (answer1?.ToString(), answer2?.ToString());
+		return (answer1.ToString(), answer2.ToString());
+	}
+
+	public static bool CheckOffset(long offset, v3i[] discs)
+	{
+		for (var i = 0; i < discs.Length; i++)
+			if ((discs[i][_startPosition] + offset + i + 1) % discs[i][_positionsCount] != 0)
+				return false;
+
+		return true;
 	}
 }
