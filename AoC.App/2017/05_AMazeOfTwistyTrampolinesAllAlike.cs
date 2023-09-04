aeb17859f794b9cbd43963c94b365c81dab653f1
filip komorovsky <filip.komorovsky@gmail.com>
2023-09-04 22:46:10 +0200

    2017/05

--- AoC.App/2017/05_AMazeOfTwistyTrampolinesAllAlike.cs
+++ AoC.App/2017/05_AMazeOfTwistyTrampolinesAllAlike.cs
@@ -2,19 +2,44 @@ using Ujeby.AoC.Common;
 
 namespace Ujeby.AoC.App._2017_05;
 
-[AoCPuzzle(Year = 2017, Day = 05, Answer1 = null, Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2017, Day = 05, Answer1 = "374269", Answer2 = "27720699", Skip = false)]
 public class AMazeOfTwistyTrampolinesAllAlike : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		// TODO 2017/05
-
 		// part1
-		string answer1 = null;
+		var i = 0L;
+		var answer1 = 0L;
+		var jumps = input.Select(x => long.Parse(x)).ToArray();
+		while (i >= 0 && i < jumps.Length)
+		{
+			var j = jumps[i];
+			
+			jumps[i]++;
+			
+			i += j;
+			answer1++;
+		}
 
 		// part2
-		string answer2 = null;
+		i = 0L;
+		var answer2 = 0L;
+		jumps = input.Select(x => long.Parse(x)).ToArray();
+		while (i >= 0 && i < jumps.Length)
+		{
+			var j = jumps[i];
+			
+			if (j >= 3)
+				jumps[i]--;
+			else
+				jumps[i]++;
 
-		return (answer1?.ToString(), answer2?.ToString());
+			i += j;
+			answer2++;
+		}
+
+		// 720 too low
+
+		return (answer1.ToString(), answer2.ToString());
 	}
 }
