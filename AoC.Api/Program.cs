24de13b7cc2efd332da45dfe92a3061bbce95e5f
filip komorovsky <filip.komorovsky@gmail.com>
2023-12-02 12:28:57 +0100

    webapi CORS

--- AoC.Api/Program.cs
+++ AoC.Api/Program.cs
@@ -17,6 +17,37 @@ namespace Ujeby.AoC.Api
 
 			var app = builder.Build();
 
+			// f**k you CORS
+			app.Use(async (context, next) =>
+			{
+				if (context.Request.Method == "OPTIONS")
+				{
+					context.Response.StatusCode = 200;
+
+					await WriteCorsAsync(context);
+
+					await context.Response.WriteAsync("");
+				}
+				else
+				{
+					context.Response.OnStarting(WriteCorsAsync, context);
+
+					await next(context);
+				}
+
+				static Task WriteCorsAsync(object state)
+				{
+					var context = (HttpContext)state;
+
+					context.Response.Headers.Add("Access-Control-Allow-Origin", context.Request.Headers.Origin);
+					context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+					context.Response.Headers.Add("Access-Control-Allow-Methods", string.Join(", ", new[] { context.Request.Method }.Union(context.Request.Headers.AccessControlRequestMethod.Select(v => v))));
+					context.Response.Headers.Add("Access-Control-Allow-Headers", string.Join(", ", context.Request.Headers.Select(p => p.Key).Union(context.Request.Headers.AccessControlRequestHeaders.Select(v => v))));
+
+					return Task.CompletedTask;
+				}
+			});
+
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
