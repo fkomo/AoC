480902355ad4e0082381c421295e4e3ad63d0458
filip komorovsky <filip.komorovsky@gmail.com>
2023-12-11 00:04:52 +0100

    basic Graph vis

--- AoC.Vis.App/2023/HauntedWasteland.cs
+++ AoC.Vis.App/2023/HauntedWasteland.cs
@@ -1,38 +1,17 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using Ujeby.AoC.Common;
+﻿using Ujeby.AoC.Common;
 using Ujeby.AoC.Vis.App.Common;
 using Ujeby.AoC.Vis.App.Ui;
 using Ujeby.Graphics;
-using Ujeby.Graphics.Entities;
 using Ujeby.Graphics.Sdl;
-using Ujeby.Tools.StringExtensions;
 using Ujeby.Vectors;
 
 namespace Ujeby.AoC.Vis.App
 {
-	public class GraphNodeMeta
-	{
-		public v2i Position { get; set; }
-	}
-
-	public class GraphNode
-	{
-		public string Id { get; set; }
-		public string[] Children { get; set; }
-
-		public GraphNodeMeta Meta { get; set; }
-
-		public GraphNode() { }
-	}
-
-	internal class HauntedWasteland : AoCRunnable
+    internal class HauntedWasteland : AoCRunnable
 	{
 		public override string Name => $"#08 {nameof(HauntedWasteland)}";
-		int _pickZone = 5;
-		string _selected = null;
-		bool _moving = false;
 
-		Dictionary<string, GraphNode> _graph;
+		Graph _graph;
 
 		public HauntedWasteland(v2i windowSize) : base(windowSize)
 		{
@@ -42,58 +21,33 @@ namespace Ujeby.AoC.Vis.App
 		protected override void Init()
 		{
 			var puzzle = new AoC.App._2023_08.HauntedWasteland();
-			var input = InputProvider.Read(AppSettings.InputDirectory, puzzle.Year, puzzle.Day, ".sample");
+			var input = InputProvider.Read(AppSettings.InputDirectory, puzzle.Year, puzzle.Day);
 
-			_graph = input.Skip(2)
+			_graph = new Graph();
+			_graph.SetData(input.Skip(2)
 				.ToDictionary(
 					x => x[..3],
 					x => new GraphNode
 					{
 						Id = x[..3],
 						Children = new string[] { x.Substring(7, 3), x.Substring(12, 3) }
-					});
+					}));
 
-			GraphLayout_Random(_graph, new v2i(-128), new v2i(128));
+			_graph.GraphLayout_Random(new v2i(-128), new v2i(128));
 		}
 
 		protected override void Update()
 		{
-			var p = Grid.MousePositionDiscrete;
-			_selected = _graph
-				.FirstOrDefault(x =>
-				{
-					var dist = (x.Value.Meta.Position - p).Abs();
-					return dist.X <= _pickZone && dist.Y <= _pickZone;
-				})
-				.Key;
-
-			if (_moving)
-			{
-				_graph[_selected].Meta.Position = p;
-			}
+			_graph.Update(Grid);
 		}
 
 		protected override void Render()
 		{
-			Grid.Draw();
+			Grid.Draw(true, false, false);
 
-			RenderGraph(_graph);
+			_graph.RenderGraph(Grid);
 
 			Grid.DrawMouseCursor(style: GridCursorStyles.FullRowColumn);
-
-			if (_selected != null)
-			{
-				var node = _graph[_selected];
-				var p = node.Meta.Position;
-				var fill = Colors.Yellow;
-				fill.W = .2;
-				Grid.DrawRect((int)p.X - _pickZone, (int)p.Y - _pickZone, (_pickZone * 2) + 1, (_pickZone * 2) + 1, Colors.Red, fill);
-
-				Sdl2Wrapper.DrawText(new v2i(32, 32), null,
-					new Text($"{nameof(_selected)}: {_selected?.ToString()}"),
-					new Text($"{nameof(GraphNodeMeta.Position)}: {_graph[_selected].Meta.Position}")
-					);
-			}
 
 			base.Render();
 		}
@@ -105,49 +59,12 @@ namespace Ujeby.AoC.Vis.App
 
 		protected override void LeftMouseDown()
 		{
-			if (_selected != null)
-			{
-				_moving = true;
-			}
+			_graph.LeftMouseDown();
 		}
 
 		protected override void LeftMouseUp()
 		{
-			if (_moving)
-			{
-				_moving = false;
-			}
-		}
-
-		private static void GraphLayout_Random(Dictionary<string, GraphNode> graph, v2i from, v2i to)
-		{
-			var rng = new Random((int)DateTime.Now.Ticks);
-			foreach (var node in graph)
-			{
-				node.Value.Meta = new GraphNodeMeta
-				{
-					Position = new v2i(rng.Next((int)from.X, (int)to.X), rng.Next((int)from.Y, (int)to.Y))
-				};
-			}
-		}
-
-		private void RenderGraph(Dictionary<string, GraphNode> graph)
-		{
-			var lineColor = Colors.DarkGray;
-			lineColor.W = .8;
-
-			foreach (var node in graph.Values)
-			{
-				foreach (var child in node.Children)
-				{
-					var from = node.Meta.Position;
-					var to = graph[child].Meta.Position;
-					Grid.DrawLine((int)from.X, (int)from.Y, (int)to.X, (int)to.Y, lineColor);
-				}
-			}
-
-			foreach (var node in graph.Values)
-				Grid.DrawCell(node.Meta.Position, Colors.White, Colors.LightGray);
+			_graph.LeftMouseUp();
 		}
 	}
 }
---
+++ AoC.Vis.App/Common/Graph.cs
@@ -0,0 +1,114 @@
+﻿using Ujeby.Graphics;
+using Ujeby.Graphics.Entities;
+using Ujeby.Graphics.Sdl;
+using Ujeby.Vectors;
+
+namespace Ujeby.AoC.Vis.App.Common
+{
+    public class GraphNodeMeta
+    {
+        public v2i Position { get; set; }
+    }
+
+    public class GraphNode
+    {
+        public string Id { get; set; }
+        public string[] Children { get; set; }
+
+        public GraphNodeMeta Meta { get; set; }
+
+        public GraphNode() { }
+    }
+
+    public class Graph
+    {
+        const int _pickZone = 5;
+        string _selected = null;
+        bool _moving = false;
+
+        public Dictionary<string, GraphNode> Data { get; private set; }
+
+        public void LeftMouseDown()
+        {
+            if (_selected != null)
+            {
+                _moving = true;
+            }
+        }
+
+        public void LeftMouseUp()
+        {
+            if (_moving)
+            {
+                _moving = false;
+            }
+        }
+
+        public void GraphLayout_Random(v2i from, v2i to)
+        {
+            var rng = new Random((int)DateTime.Now.Ticks);
+            foreach (var node in Data)
+            {
+                node.Value.Meta = new GraphNodeMeta
+                {
+                    Position = new v2i(rng.Next((int)from.X, (int)to.X), rng.Next((int)from.Y, (int)to.Y))
+                };
+            }
+        }
+
+        public void RenderGraph(WorkspaceGrid grid)
+        {
+            var lineColor = Colors.DarkGray;
+            lineColor.W = .8;
+
+            foreach (var node in Data.Values)
+            {
+                foreach (var child in node.Children)
+                {
+                    var from = node.Meta.Position;
+                    var to = Data[child].Meta.Position;
+                    grid.DrawLine((int)from.X, (int)from.Y, (int)to.X, (int)to.Y, lineColor);
+                }
+            }
+
+            foreach (var node in Data.Values)
+                grid.DrawCell(node.Meta.Position, Colors.White, Colors.LightGray);
+
+            if (_selected != null)
+            {
+                var node = Data[_selected];
+                var p = node.Meta.Position;
+                var fill = Colors.Yellow;
+                fill.W = .2;
+                grid.DrawRect((int)p.X - _pickZone, (int)p.Y - _pickZone, _pickZone * 2 + 1, _pickZone * 2 + 1, Colors.Red, fill);
+
+                Sdl2Wrapper.DrawText(new v2i(32, 32), null,
+                    new Text($"{nameof(_selected)}: {_selected?.ToString()}"),
+                    new Text($"{nameof(GraphNodeMeta.Position)}: {Data[_selected].Meta.Position}")
+                    );
+            }
+        }
+
+        internal void SetData(Dictionary<string, GraphNode> data)
+        {
+            Data = data;
+        }
+
+        internal void Update(WorkspaceGrid grid)
+        {
+            var p = grid.MousePositionDiscrete;
+            _selected = Data
+                .FirstOrDefault(x =>
+                {
+                    var dist = (x.Value.Meta.Position - p).Abs();
+                    return dist.X <= _pickZone && dist.Y <= _pickZone;
+                })
+                .Key;
+
+            if (_moving)
+            {
+                Data[_selected].Meta.Position = p;
+            }
+        }
+    }
+}
