485816d3cb9e517bb8427a6077b0ceb866b49c7a
filip komorovsky <filip.komorovsky@gmail.com>
2023-12-13 12:04:01 +0100

    2023/12 p2 (memoization ftw!)

--- AoC.App/2023/12_HotSprings.cs
+++ AoC.App/2023/12_HotSprings.cs
@@ -3,47 +3,52 @@ using Ujeby.Tools.StringExtensions;
 
 namespace Ujeby.AoC.App._2023_12;
 
-[AoCPuzzle(Year = 2023, Day = 12, Answer1 = "7084", Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2023, Day = 12, Answer1 = "7084", Answer2 = "8414003326821", Skip = false)]
 public class HotSprings : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		var records = input
-			.Select(x => (Mask: x[..x.IndexOf(' ')], DmgGroups: x.ToNumArray()))
-			.ToArray();
-
 		// part1
+		_cache.Clear();
 		long answer1 = 0;
-		foreach (var (Mask, DmgGroups) in records)
+		for (var i = 0; i < input.Length; i++)
 		{
-			Debug.Line($"{Mask} {string.Join(',', DmgGroups)}");
-			var count = PossibleArrangements(Mask, DmgGroups);
-			Debug.Line($"{count}{Environment.NewLine}");
+			var record = input[i];
+
+			Debug.Line($"* {record}");
+			var count = PossibleArrangements(i, record);
+			Debug.Line($"  {count}{Environment.NewLine}");
 			answer1 += count;
 		}
 
 		// part2
-		string answer2 = null;
+		_cache.Clear();
+		long answer2 = 0;
+		for (var i = 0; i < input.Length; i++)
+			answer2 += PossibleArrangementsUnfolded(i, input[i]);
 
-		return (answer1.ToString(), answer2?.ToString());
+		return (answer1.ToString(), answer2.ToString());
 	}
 
-	static long PossibleArrangements(string mask, long[] dmgGroups, 
-		string tmp = "", int maskIdx = 0, int dmgGrpIdx = 0)
+	static long PossibleArrangements(int id, string record)
+		=> PossibleArrangements(id, record[..record.IndexOf(' ')], record.ToNumArray());
+
+	static long PossibleArrangementsUnfolded(int id, string record)
+		=> PossibleArrangements(id,
+			string.Join('?', Enumerable.Repeat(record[..record.IndexOf(' ')], 5)),
+			string.Join(',', Enumerable.Repeat(record[(record.IndexOf(' ') + 1)..], 5)).ToNumArray());
+
+	static Dictionary<(int, int, int), long> _cache = new();
+
+	static long PossibleArrangements(int id, string mask, long[] dmgGroups, 
+		int maskIdx = 0, int dmgGrpIdx = 0)
 	{
+		var cacheKey = (id, maskIdx, dmgGrpIdx);
+		if (_cache.ContainsKey(cacheKey))
+			return _cache[cacheKey];
+
 		if (dmgGrpIdx == dmgGroups.Length)
-		{
-			Debug.Line($"{tmp.PadRight(mask.Length, '.')}");
-			if (maskIdx < mask.Length && mask[maskIdx..].Contains('#'))
-				return 0;
-
-			// final check
-			for (var i = 0; i < mask.Length; i++)
-				if (mask[i] == '#' && tmp[i] != '#')
-					return 0;
-
-			return 1;
-		}
+			return (maskIdx < mask.Length && mask[maskIdx..].Contains('#')) ? 0 : 1;
 
 		var to = mask.Length - (dmgGroups.Skip(dmgGrpIdx).Sum() + (dmgGroups.Length - dmgGrpIdx - 1));
 		var dmg = (int)dmgGroups[dmgGrpIdx];
@@ -54,29 +59,31 @@ public class HotSprings : PuzzleBase
 			if (maskIdx + dmg > mask.Length)
 				continue;
 
-			var valid = true;
+			var free = true;
 			for (var x = i; x < i + dmg; x++)
 			{
 				if (mask[x] == '.')
 				{
-					valid = false;
+					free = false;
 					break;
 				}
 			}
-			if (!valid || (valid && i + dmg < mask.Length && mask[i + dmg] == '#'))
+			if (!free || (free && i + dmg < mask.Length && mask[i + dmg] == '#'))
+			{
+				if (mask[i] == '#')
+					break;
+
 				continue;
+			}
 
-			var _tmp = tmp + new string(Enumerable.Repeat('.', i - maskIdx).ToArray()) + new string(Enumerable.Repeat('#', dmg).ToArray());
-			if (i + dmg < mask.Length)
-				_tmp += '.';
-
-			count += PossibleArrangements(mask, dmgGroups, 
-				tmp: _tmp,
+			count += PossibleArrangements(id, mask, dmgGroups, 
 				maskIdx: i + dmg + 1, dmgGrpIdx: dmgGrpIdx + 1);
 
 			if (mask[i] == '#')
 				break;
 		}
+
+		_cache.Add(cacheKey, count);
 
 		return count;
 	}
