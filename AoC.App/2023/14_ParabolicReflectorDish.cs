2d4d307421838458c2e21e0a699bd0f70419ce39
filip komorovsky <filip.komorovsky@gmail.com>
2023-12-15 01:40:44 +0100

    2023/14 p2

--- AoC.App/2023/14_ParabolicReflectorDish.cs
+++ AoC.App/2023/14_ParabolicReflectorDish.cs
@@ -1,33 +1,135 @@
 using Ujeby.AoC.Common;
+using Ujeby.Vectors;
 
 namespace Ujeby.AoC.App._2023_14;
 
-[AoCPuzzle(Year = 2023, Day = 14, Answer1 = "109098", Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2023, Day = 14, Answer1 = "109098", Answer2 = "100064", Skip = false)]
 public class ParabolicReflectorDish : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		// part1
-		long answer1 = 0;
-		var stack = Enumerable.Repeat(-1, input[0].Length).ToArray();
-		for (var i = 0; i < input.Length; i++)
-		{
-			for (var x = 0; x < input[i].Length; x++)
-			{
-				if (input[i][x] == '#')
-					stack[x] = i;
+		var dish = input.Select(x => x.Select(y => y).ToArray()).ToArray();
 
-				else if (input[i][x] == 'O')
+#if DEBUG
+		foreach (var line in dish)
+			Debug.Line(new string(line));
+		Debug.Line();
+#endif
+		// part1
+		var answer1 = NorthLoad(dish);
+
+		// part2
+		var invalidRock = new v2i(-1);
+		var rocks = input
+			.SelectMany((row, y) => row.Select((col, x) => (Pos: new v2i(x, y), Shape: col)))
+			.Where(x => x.Shape != '.')
+			.ToArray();
+		Debug.Line($"{rocks.Length} rocks [#,O]");
+
+		var loads = new List<long>();
+		var loadsHash = new HashSet<long>();
+
+		var loopStart = -1;
+		var loopStartHashCount = 0;
+
+		var cycles = 200;
+		while (cycles-- > 0)
+		{
+			// tilt north
+			var stack = Enumerable.Repeat((long)-1, dish.Length).ToArray();
+			rocks = rocks.OrderBy(x => x.Pos.Y).ToArray();
+			for (var i = 0; i < rocks.Length; i++)
+			{
+				var rock = rocks[i];
+				if (rocks[i].Shape == '#')
+					stack[rock.Pos.X] = rock.Pos.Y;
+				else if (rocks[i].Shape == 'O')
+					rocks[i].Pos.Y = ++stack[rock.Pos.X];
+			}
+
+			// tilt west
+			stack = Enumerable.Repeat((long)-1, dish.Length).ToArray();
+			rocks = rocks.OrderBy(x => x.Pos.X).ToArray();
+			for (var i = 0; i < rocks.Length; i++)
+			{
+				var rock = rocks[i];
+				if (rocks[i].Shape == '#')
+					stack[rock.Pos.Y] = rock.Pos.X;
+				else if (rocks[i].Shape == 'O')
+					rocks[i].Pos.X = ++stack[rock.Pos.Y];
+			}
+
+			// tilt south
+			stack = Enumerable.Repeat((long)dish.Length, dish.Length).ToArray();
+			rocks = rocks.OrderByDescending(x => x.Pos.Y).ToArray();
+			for (var i = 0; i < rocks.Length; i++)
+			{
+				var rock = rocks[i];
+				if (rocks[i].Shape == '#')
+					stack[rock.Pos.X] = rock.Pos.Y;
+				else if (rocks[i].Shape == 'O')
+					rocks[i].Pos.Y = --stack[rock.Pos.X];
+			}
+
+			// tilt east
+			stack = Enumerable.Repeat((long)dish.Length, dish.Length).ToArray();
+			rocks = rocks.OrderByDescending(x => x.Pos.X).ToArray();
+			for (var i = 0; i < rocks.Length; i++)
+			{
+				var rock = rocks[i];
+				if (rocks[i].Shape == '#')
+					stack[rock.Pos.Y] = rock.Pos.X;
+				else if (rocks[i].Shape == 'O')
+					rocks[i].Pos.X = --stack[rock.Pos.Y];
+			}
+
+			var northLoad = NorthLoad(rocks, input.Length);
+			loads.Add(northLoad);
+			if (!loadsHash.Add(northLoad))
+			{
+				Debug.Line($"#{loads.Count} northLoad: {northLoad} hashCount: {loadsHash.Count}");
+				if (loopStartHashCount != loadsHash.Count)
 				{
-					stack[x]++;
-					answer1 += input.Length - stack[x];
+					loopStartHashCount = loadsHash.Count;
+					loopStart = loads.Count - 1;
 				}
 			}
 		}
 
-		// part2
-		string answer2 = null;
+		var loadsArray = loads.ToArray();
+		var loopLength = Array.IndexOf(loadsArray, loadsArray[loopStart], loopStart + 1) - loopStart;
+		var loop = loadsArray
+			.Skip(loopStart).Take(loopLength)
+			.ToArray();
 
-		return (answer1.ToString(), answer2?.ToString());
+		var answer2 = loop[(1000000000 - loopStart) % loopLength - 1];
+
+		return (answer1.ToString(), answer2.ToString());
+	}
+
+	static long NorthLoad((v2i Pos, char Shape)[] rocks, int dishSize)
+		=> rocks
+			.Where(x => x.Shape == 'O')
+			.Sum(x => dishSize - x.Pos.Y);
+
+	static long NorthLoad(char[][] dish)
+	{
+		long load = 0;
+		var northStack = Enumerable.Repeat(-1, dish.Length).ToArray();
+		for (var i = 0; i < dish.Length; i++)
+		{
+			for (var x = 0; x < dish[i].Length; x++)
+			{
+				if (dish[i][x] == '#')
+					northStack[x] = i;
+				else if (dish[i][x] == 'O')
+				{
+					northStack[x]++;
+					load += dish.Length - northStack[x];
+				}
+			}
+		}
+
+		return load;
 	}
 }
