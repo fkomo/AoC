97e2d372a7d573e552bb026b2e944ec867eecd78
filip komorovsky <filip.komorovsky@gmail.com>
2023-12-26 20:13:38 +0100

    2023/17 p1+p2 + refactoring

--- AoC.App/2023/17_ClumsyCrucible.cs
+++ AoC.App/2023/17_ClumsyCrucible.cs
@@ -1,115 +1,134 @@
-﻿using System.Text;
-using Ujeby.AoC.Common;
+﻿using Ujeby.AoC.Common;
 using Ujeby.Vectors;
 
 namespace Ujeby.AoC.App._2023_17;
 
-[AoCPuzzle(Year = 2023, Day = 17, Answer1 = null, Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2023, Day = 17, Answer1 = "1155", Answer2 = "1283", Skip = false)]
 public class ClumsyCrucible : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		var grid = input.Select(y => y.Select(x => x - '0').ToArray()).ToArray();
+		var grid = input
+			.Select(y => y.Select(x => x - '0').ToArray())
+			.ToArray();
 
 		// part1
-		long? answer1 = null;
-		// TODO 2023/17 p1
-
-		//var answer1 = MinHeathLoss(grid, new v2i[] { v2i.Zero }, Array.Empty<int>(),
-		//	minHeatLoss: 2 * input.Length * 9
-		//	);
+		var answer1 = MinHeatLoss(grid, new v2i(0), new v2i(grid.Length - 1));
+		// TODO 2023/17 OPTIMIZE p1 (12s)
 
 		// part2
-		long? answer2 = null;
-		// TODO 2023/17 p2
+		var answer2 = MinHeatLoss2(grid, new v2i(0), new v2i(grid.Length - 1));
+		// TODO 2023/17 OPTIMIZE p2 (2min)
 
-		return (answer1?.ToString(), answer2?.ToString());
+		return (answer1.ToString(), answer2.ToString());
 	}
 
-	static readonly Dictionary<(v2i Pos, int IncomingDir), long> _cache = new();
+	record class State(v2i Pos, v2i Dir, int Dist);
 
-	/// <summary>
-	/// TODO result is wrong, looks like it counts even first block (0,0)
-	/// </summary>
-	/// <param name="grid"></param>
-	/// <param name="path"></param>
-	/// <param name="last3"></param>
-	/// <param name="minHeatLoss"></param>
-	/// <param name="heatLoss"></param>
-	/// <returns></returns>
-	static long MinHeathLoss(int[][] grid, v2i[] path, int[] last3, 
-		long minHeatLoss = long.MaxValue, long heatLoss = 0)
+	static long MinHeatLoss(int[][] grid, v2i source, v2i target)
 	{
-		var p = path[^1];
-		heatLoss += grid[p.Y][p.X];
-		if (heatLoss > minHeatLoss)
-			return long.MaxValue;
+		var targetCost = long.MaxValue;
+		var stateQueue = new List<(State State, long Cost)>();
+		var visitedStates = new HashSet<State>();
 
-		var heatLossToEnd = long.MaxValue;
-		if (p.X == grid.Length - 1 && p.Y == grid.Length - 1)
+		bool AddState(long cost, v2i p, v2i dir, int distance, out long targetCost)
 		{
-			Debug.Line($"heatLoss: {heatLoss}");
-			DebugGridPath(grid, path);
+			p += dir;
 
-			return grid[p.Y][p.X];
+			targetCost = cost;
+			if (p.X < 0 || p.Y < 0 || p.X >= grid.Length || p.Y >= grid.Length)
+				return false;
+
+			cost += grid[p.Y][p.X];
+			if (p == target)
+			{
+				targetCost = cost;
+				return true;
+			}
+
+			var state = new State(p, dir, distance);
+			if (visitedStates.Add(state))
+				stateQueue.Add((state, cost));
+
+			return false;
 		}
 
-		//if (last3.Any() && _cache.TryGetValue((path[^1], last3[^1]), out long cachedHeatLoss))
-		//	return cachedHeatLoss;
-
-		for (var i = 0; i < v2i.UpDownLeftRight.Length; i++) 
+		AddState(0, source, v2i.Up, 1, out _);
+		AddState(0, source, v2i.Right, 1, out _);
+		while (stateQueue.Any())
 		{
-			// max 3 blocks in single direction
-			if (last3.Length == 3 && last3[0] == last3[1] && last3[1] == last3[2] && last3[2] == i)
-				continue;
+			var minCost = stateQueue.Min(x => x.Cost);
+			var nextStates = stateQueue.Where(x => x.Cost == minCost).ToArray();
+			stateQueue.RemoveAll(x => nextStates.Contains(x));
 
-			var p2 = p + v2i.UpDownLeftRight[i];
-
-			// out of bounds
-			if (p2.X < 0 || p2.Y < 0 || p2.X >= grid.Length || p2.Y >= grid.Length)
-				continue;
-
-			// not visiting same block twice (so no coming back)
-			if (path.Contains(p2))
-				continue;
-
-			var mhl = MinHeathLoss(grid, 
-				path.Concat(new v2i[] { p2 }).ToArray(),
-				last3.Skip(last3.Length == 3 ? 1 : 0).Concat(new int[] { i }).ToArray(),
-				minHeatLoss: minHeatLoss,
-				heatLoss: heatLoss);
-			if (mhl < heatLossToEnd)
+			foreach (var state in nextStates)
 			{
-				heatLossToEnd = mhl;
-				minHeatLoss = heatLoss + heatLossToEnd;
+				if (AddState(minCost, state.State.Pos, new v2i(state.State.Dir.Y, -state.State.Dir.X), 1, out targetCost) ||
+					AddState(minCost, state.State.Pos, new v2i(-state.State.Dir.Y, state.State.Dir.X), 1, out targetCost))
+					return targetCost;
+
+				if (state.State.Dist < 3)
+					if (AddState(minCost, state.State.Pos, state.State.Dir, state.State.Dist + 1, out targetCost))
+						return targetCost;
 			}
 		}
 
-		if (heatLossToEnd == long.MaxValue)
-			return long.MaxValue;
-
-		heatLossToEnd += grid[p.Y][p.X];
-		//if (last3.Any() && minHeatLoss < 2 * grid.Length * 9)
-		//	_cache.Add((path[^1], last3[^1]), heatLossToEnd);
-
-		return heatLossToEnd;
+		return targetCost;
 	}
 
-	static void DebugGridPath(int[][] grid, v2i[] path)
+	static long MinHeatLoss2(int[][] grid, v2i source, v2i target)
 	{
-		var sb = new StringBuilder();
-		for (var y  = 0; y < grid.Length; y++)
+		var targetCost = long.MaxValue;
+		var stateQueue = new List<(State State, long Cost)>();
+		var visitedStates = new HashSet<State>();
+
+		bool AddState(long cost, v2i p, v2i dir, int distance, out long targetCost)
 		{
-			sb.Clear();
-			for (var x = 0; x < grid[y].Length; x++)
+			p += dir;
+
+			targetCost = cost;
+			if (p.X < 0 || p.Y < 0 || p.X >= grid.Length || p.Y >= grid.Length)
+				return false;
+
+			cost += grid[p.Y][p.X];
+			if (p == target && distance >= 4 && distance <= 10)
 			{
-				if (path.Contains(new v2i(x, y)))
-					sb.Append('█');
-				else
-					sb.Append(grid[y][x]);
+				targetCost = cost;
+				return true;
 			}
-			Debug.Line(sb.ToString());
+
+			var state = new State(p, dir, distance);
+			if (visitedStates.Add(state))
+				stateQueue.Add((state, cost));
+
+			return false;
 		}
-		Debug.Line();
+
+		AddState(0, source, v2i.Up, 1, out _);
+		AddState(0, source, v2i.Right, 1, out _);
+		while (stateQueue.Any())
+		{
+			var minCost = stateQueue.Min(x => x.Cost);
+			var nextStates = stateQueue.Where(x => x.Cost == minCost).ToArray();
+			stateQueue.RemoveAll(x => nextStates.Contains(x));
+
+			foreach (var state in nextStates)
+			{
+				if (state.State.Dist >= 4)
+				{
+					if (AddState(minCost, state.State.Pos, new v2i(state.State.Dir.Y, -state.State.Dir.X), 1, out targetCost) ||
+						AddState(minCost, state.State.Pos, new v2i(-state.State.Dir.Y, state.State.Dir.X), 1, out targetCost))
+						return targetCost;
+				}
+
+				if (state.State.Dist < 10)
+				{
+					if (AddState(minCost, state.State.Pos, state.State.Dir, state.State.Dist + 1, out targetCost))
+						return targetCost;
+				}
+			}
+		}
+
+		return targetCost;
 	}
 }
