67d26f83752ab347291470ab8915687797f9b7ad
filip komorovsky <filip.komorovsky@gmail.com>
2023-12-26 01:47:04 +0100

    2023/25 refactoring + Parallel

--- AoC.App/2023/25_Snowverload.cs
+++ AoC.App/2023/25_Snowverload.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using Ujeby.Alg;
 using Ujeby.AoC.Common;
 
@@ -17,92 +18,41 @@ public class Snowverload : PuzzleBase
 		var graph = input
 			.ToDictionary(
 				x => Array.IndexOf(nodes, x[..3]), 
-				x => x[5..].Split(' ').Select(n => (Idx: Array.IndexOf(nodes, n), Weight: 1L)).ToArray());
-		Debug.Line($"{graph.Sum(x => x.Value.Length)} connections");
+				x => x[5..].Split(' ').Select(n => (Dest: Array.IndexOf(nodes, n), Weight: 1L)).ToArray())
+			.Fix1WayAs2Way();
 
-		var graph2way = Fix1WayGraphAs2Way(graph);
-		Debug.Line($"{graph2way.Sum(x => x.Value.Length)} 2way connections");
+		Debug.Line($"{graph.Sum(x => x.Value.Length)} connections");
 
 		// part1
 		// sample paths between random nodes and find most used
-		var segmentUsage = new Dictionary<(int From, int To), long>();
-		var rnd = new Random();
-		for (var i = 0; i < 100; i++)
+		var rnd = new Random(0);
+		var segmentsBag = new ConcurrentBag<(int From, int To)>();
+		Parallel.For(0, 100, (i) =>
 		{
 			var n1 = rnd.Next(nodes.Length);
 			var n2 = rnd.Next(nodes.Length);
+			var sp = Dijkstra.ShortestPath(graph, graph.Keys.ToArray(), n1, n2);
+			for (var p = 0; p < sp.Length - 1; p++)
+				segmentsBag.Add((System.Math.Min(sp[p], sp[p + 1]), System.Math.Max(sp[p], sp[p + 1])));
+		});
 
-			var sp = Dijkstra.ShortestPath(graph2way, graph2way.Keys.ToArray(), n1, n2);
-			if (sp.Length > 1)
-			{
-				for (var p = 0; p < sp.Length - 1; p++)
-				{
-					var key = (System.Math.Min(sp[p], sp[p + 1]), System.Math.Max(sp[p], sp[p + 1]));
-					if (!segmentUsage.ContainsKey(key))
-						segmentUsage.Add(key, 1);
-					else
-						segmentUsage[key]++;
-				}
-			}
-		}
-
-		// remove 3 most used connections
-		var top3 = segmentUsage
-			.OrderByDescending(x => x.Value)
+		// remove 3 most used connections from graph
+		var top3 = segmentsBag
+			.GroupBy(x => x)
+			.OrderByDescending(x => x.Count())
 			.Take(3);
 		foreach (var s in top3)
 		{
-			graph2way[s.Key.From] = graph2way[s.Key.From].Where(x => x.Idx != s.Key.To).ToArray();
-			graph2way[s.Key.To] = graph2way[s.Key.To].Where(x => x.Idx != s.Key.From).ToArray();
+			graph[s.Key.From] = graph[s.Key.From].Where(x => x.Dest != s.Key.To).ToArray();
+			graph[s.Key.To] = graph[s.Key.To].Where(x => x.Dest != s.Key.From).ToArray();
 		}
 
-		var count = CountNodes(graph2way, graph2way.Keys.First());
-		var answer1 = (graph2way.Keys.Count - count) * count;
+		var count = graph.CountReach(graph.Keys.First());
+		var answer1 = (graph.Keys.Count - count) * count;
 
 		// part2
 		string answer2 = "*";
 
 		return (answer1.ToString(), answer2.ToString());
-	}
-
-	static int CountNodes(Dictionary<int, (int Idx, long Weight)[]> graph, int source)
-	{
-		var counted = new HashSet<int>();
-
-		var nodeQueue = new Queue<int>();
-		nodeQueue.Enqueue(source);
-		while (nodeQueue.Any())
-		{
-			var node = nodeQueue.Dequeue();
-			if (!counted.Add(node))
-				continue;
-
-			foreach (var (Idx, Weight) in graph[node].Where(x => !counted.Contains(x.Idx)))
-				nodeQueue.Enqueue(Idx);
-		}
-
-		return counted.Count;
-	}
-
-	static Dictionary<int, (int Idx, long Weight)[]> Fix1WayGraphAs2Way(Dictionary<int, (int Idx, long Weight)[]> graph)
-	{
-		var fix = new Dictionary<int, HashSet<(int, long)>>();
-		foreach (var from in graph)
-		{
-			if (!fix.ContainsKey(from.Key))
-				fix.Add(from.Key, new());
-
-			foreach (var to in from.Value)
-			{
-				fix[from.Key].Add((to.Idx, to.Weight));
-
-				if (!fix.ContainsKey(to.Idx))
-					fix.Add(to.Idx, new());
-
-				fix[to.Idx].Add((from.Key, to.Weight));
-			}
-		}
-
-		return fix.ToDictionary(x => x.Key, x => x.Value.ToArray());
 	}
 }
