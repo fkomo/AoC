5b8b21f671328e57e4df6358b01281e05b1846ba
filip komorovsky <filip.komorovsky@gmail.com>
2024-11-07 12:12:35 +0100

    2017/10

--- AoC.App/2017/10_KnotHash.cs
+++ AoC.App/2017/10_KnotHash.cs
@@ -1,20 +1,61 @@
 using Ujeby.AoC.Common;
+using Ujeby.Tools.StringExtensions;
 
 namespace Ujeby.AoC.App._2017_10;
 
-[AoCPuzzle(Year = 2017, Day = 10, Answer1 = null, Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2017, Day = 10, Answer1 = "54675", Answer2 = "a7af2706aa9a09cf5d848c1e6605dd2a", Skip = false)]
 public class KnotHash : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		// TODO 2017/10
-
 		// part1
-		string answer1 = null;
+		var elements = SparseHash(
+			Enumerable.Range(0, 256).Select(x => (byte)x).ToArray(), 
+			input[0].ToNumArray().Select(x => (byte)x).ToArray(),
+			rounds: 1);
+		
+		var answer1 = elements[0] * elements[1];
 
 		// part2
-		string answer2 = null;
+		var sparseHash = SparseHash(
+			Enumerable.Range(0, 256).Select(x => (byte)x).ToArray(),
+			input[0].Select(x => (byte)x).Concat(new byte[] { 17, 31, 73, 47, 23 }).ToArray(),
+			rounds: 64);
 
-		return (answer1?.ToString(), answer2?.ToString());
+		var answer2 = string.Concat(DenseHash(sparseHash).Select(x => x.ToString("x2")));
+
+		return (answer1.ToString(), answer2.ToString());
+	}
+
+	static byte[] DenseHash(byte[] elements)
+	{
+		var result = new byte[16];
+		for (var i = 0; i < 16; i++)
+			result[i] = elements.Skip(i * 16).Take(16).Aggregate((x, y) => (byte)(x ^ y));
+
+		return result;
+	}
+
+	static byte[] SparseHash(byte[] elements, byte[] lengths, int rounds = 1)
+	{
+		var skip = 0;
+		var position = 0;
+
+		for (var round = 0; round < rounds; round++)
+		{
+			for (var len = 0; len < lengths.Length; len++, skip++)
+			{
+				for (var i = 0; i < lengths[len] / 2; i++)
+				{
+					var left = (position + i) % elements.Length;
+					var right = (position + lengths[len] - 1 - i) % elements.Length;
+					(elements[right], elements[left]) = (elements[left], elements[right]);
+				}
+
+				position += (lengths[len] + skip) % elements.Length;
+			}
+		}
+
+		return elements;
 	}
 }
