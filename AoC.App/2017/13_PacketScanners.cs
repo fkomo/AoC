971119c57b39fdb1a9f7ffb0400e8b72cf11d1d1
filip komorovsky <filip.komorovsky@gmail.com>
2024-11-10 14:25:53 +0100

    2017/13

--- AoC.App/2017/13_PacketScanners.cs
+++ AoC.App/2017/13_PacketScanners.cs
@@ -1,20 +1,50 @@
 using Ujeby.AoC.Common;
+using Ujeby.Tools.StringExtensions;
+using Ujeby.Vectors;
 
 namespace Ujeby.AoC.App._2017_13;
 
-[AoCPuzzle(Year = 2017, Day = 13, Answer1 = null, Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2017, Day = 13, Answer1 = "2508", Answer2 = "3913186", Skip = false)]
 public class PacketScanners : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		// TODO 2017/13
+		var layers = input
+			.Select(x =>
+			{
+				var xy = x.ToNumArray();
+				return new v3i(xy[0], xy[1], (xy[1] - 1) * 2);
+			})
+			.ToArray();
 
 		// part1
-		string answer1 = null;
+		var answer1 = layers
+			.Where(x => (x[0] % x[2]) == 0)
+			.Sum(x => x[0] * x[1]);
 
 		// part2
-		string answer2 = null;
+		var delay = 0;
+		while (++delay > 0)
+		{
+			//if (!layers.Any(x => (x[0] + delay) % (x[2]) == 0))
+			//	break;
 
-		return (answer1?.ToString(), answer2?.ToString());
+			var hit = false;
+			for (var i = 0; i < layers.Length; i++)
+			{
+				var layer = layers[i];
+				if (((layer[0] + delay) % layer[2]) == 0)
+				{
+					hit = true;
+					break;
+				}
+			}
+
+			if (!hit)
+				break;
+		}
+		var answer2 = delay;
+
+		return (answer1.ToString(), answer2.ToString());
 	}
 }
