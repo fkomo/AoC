b577aa7e60fcd59bc80980139d7be92ce21ffece
filip komorovsky <filip.komorovsky@gmail.com>
2024-11-20 21:00:46 +0100

    2018/02

--- AoC.App/2018/02_InventoryManagementSystem.cs
+++ AoC.App/2018/02_InventoryManagementSystem.cs
@@ -2,19 +2,53 @@ using Ujeby.AoC.Common;
 
 namespace Ujeby.AoC.App._2018_02;
 
-[AoCPuzzle(Year = 2018, Day = 02, Answer1 = null, Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2018, Day = 02, Answer1 = "6175", Answer2 = "asgwjcmzredihqoutcylvzinx", Skip = false)]
 public class InventoryManagementSystem : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		// TODO 2018/02
+		var grpd = input.Select(x => x.GroupBy(xx => xx)).ToArray();
 
 		// part1
-		string answer1 = null;
+		var answer1 = grpd.Count(x => x.Any(xx => xx.Count() == 2)) * grpd.Count(x => x.Any(xx => xx.Count() == 3));
 
 		// part2
-		string answer2 = null;
+		var hs = new HashSet<string>();
+		for (var s1 = 0; s1 < input.Length; s1++)
+		{
+			for (var s2 = 0; s2 < s1; s2++)
+			{
+				if (CountDiff(input[s1], input[s2]) == 1)
+				{
+					hs.Add(input[s1]);
+					hs.Add(input[s2]);
+				}
+			}
+		}
 
-		return (answer1?.ToString(), answer2?.ToString());
+		var final = hs.ToArray();
+		var answer2 = string.Empty;
+		for (var i = 0; i < final[0].Length; i++)
+		{
+			if (final[0][i] == final[1][i])
+				answer2 += final[0][i];
+		}
+
+		return (answer1.ToString(), answer2.ToString());
+	}
+
+	static int CountDiff(string a, string b)
+	{
+		if (a.Length != b.Length)
+			return -1;
+
+		var cnt = 0;
+		for (var i = 0; i < a.Length; i++)
+		{
+			if (a[i] != b[i])
+				cnt++;
+		}
+
+		return cnt;
 	}
 }
