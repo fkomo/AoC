2006f83089948a7baedcfb44967088299c1085f2
filip komorovsky <filip.komorovsky@gmail.com>
2024-11-26 00:16:11 +0100

    2018/05

--- AoC.App/2018/05_AlchemicalReduction.cs
+++ AoC.App/2018/05_AlchemicalReduction.cs
@@ -2,19 +2,66 @@ using Ujeby.AoC.Common;
 
 namespace Ujeby.AoC.App._2018_05;
 
-[AoCPuzzle(Year = 2018, Day = 05, Answer1 = null, Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2018, Day = 05, Answer1 = "9116", Answer2 = "6890", Skip = true)]
 public class AlchemicalReduction : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		// TODO 2018/05
+		var polymer = input.Single().ToCharArray();
 
 		// part1
-		string answer1 = null;
+		var answer1 = React([..polymer]);
 
 		// part2
-		string answer2 = null;
+		// TODO 2018/05 OPTIMIZE p2 (6s)
+		var answer2 = Enumerable.Range('a', 'z' - 'a')
+			.Min(x => React(polymer.Where(xx => char.ToLower(xx) != (char)x).ToArray()));
 
-		return (answer1?.ToString(), answer2?.ToString());
+		return (answer1.ToString(), answer2.ToString());
 	}
+
+	static int React(char[] polymer)
+	{
+		var polymerLength = polymer.Length;
+
+		bool change;
+		do
+		{
+			change = false;
+
+			for (var i1 = 0; i1 < polymer.Length; i1++)
+			{
+				for (; i1 < polymer.Length && polymer[i1] == 0; i1++)
+				{
+				}
+
+				if (i1 >= polymer.Length)
+					break;
+
+				var i2 = i1 + 1;
+				for (; i2 < polymer.Length && polymer[i2] == 0; i2++)
+				{
+				}
+
+				if (i2 >= polymer.Length)
+					break;
+
+				if (!CheckReaction(polymer[i1], polymer[i2]))
+					continue;
+
+				change = true;
+
+				polymer[i1] = (char)0;
+				polymer[i2] = (char)0;
+
+				i1 = i2 - 1;
+				polymerLength -= 2;
+			}
+		}
+		while (change);
+
+		return polymerLength;
+	}
+
+	static bool CheckReaction(char a, char b) => System.Math.Abs(a - b) == ('a' - 'A');
 }
