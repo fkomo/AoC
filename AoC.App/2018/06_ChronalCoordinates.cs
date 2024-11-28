e311c3ba7c459a561ef75502a4609947da49a005
filip komorovsky <filip.komorovsky@gmail.com>
2024-11-28 21:40:10 +0100

    2018/06 p2

--- AoC.App/2018/06_ChronalCoordinates.cs
+++ AoC.App/2018/06_ChronalCoordinates.cs
@@ -4,38 +4,39 @@ using Ujeby.Vectors;
 
 namespace Ujeby.AoC.App._2018_06;
 
-[AoCPuzzle(Year = 2018, Day = 06, Answer1 = "5333", Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2018, Day = 06, Answer1 = "5333", Answer2 = "35334", Skip = false)]
 public class ChronalCoordinates : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		var coords = input.Select((x,i) => (id: (char)('A' + i), cc: new v2i(x.ToNumArray()))).ToArray();
+		var coords = input.Select((x,i) => (Name: (char)('A' + i), Pos: new v2i(x.ToNumArray()))).ToArray();
 		var view = new aab2i(
-			new v2i(coords.Min(x => x.cc.X), coords.Min(x => x.cc.Y)), 
-			new v2i(coords.Max(x => x.cc.X), coords.Max(x => x.cc.Y)));
+			new v2i(coords.Min(x => x.Pos.X), coords.Min(x => x.Pos.Y)), 
+			new v2i(coords.Max(x => x.Pos.X), coords.Max(x => x.Pos.Y)));
+		var pointsInView = view.EnumPoints().ToArray();
 
 		// part1
-		var possibleCoords = coords
-			.Where(x => view.Min.X != x.cc.X && view.Max.X != x.cc.X && view.Min.Y != x.cc.Y && view.Max.Y != x.cc.Y)
-			.Select(x => x.id)
-			.ToArray();
-
-		var areas = coords.ToDictionary(x => x.id, x => 1);
-		foreach (var p in view.EnumPoints())
+		var areas = coords.ToDictionary(x => x.Name, x => 1);
+		foreach (var p in pointsInView)
 		{
-			if (coords.Any(x => x.cc == p))
+			if (coords.Any(x => x.Pos == p))
 				continue;
 
-			var distances = coords.ToDictionary(x => x.id, x => v2i.ManhDistance(p, x.cc)).OrderBy(x => x.Value).ToArray();
+			var distances = coords.ToDictionary(x => x.Name, x => v2i.ManhDistance(p, x.Pos)).OrderBy(x => x.Value).ToArray();
 			if (distances[0].Value != distances[1].Value)
 				areas[distances[0].Key]++;
 		}
-
+		
+		var possibleCoords = coords
+			.Where(x => view.Min.X != x.Pos.X && view.Max.X != x.Pos.X && view.Min.Y != x.Pos.Y && view.Max.Y != x.Pos.Y)
+			.Select(x => x.Name)
+			.ToArray(); 
+		
 		var answer1 = areas.Where(x => possibleCoords.Contains(x.Key)).Max(x => x.Value);
 
 		// part2
-		string answer2 = null;
+		var answer2 = pointsInView.Count(x => coords.Sum(xx => v2i.ManhDistance(x, xx.Pos)) < 10_000);
 
-		return (answer1.ToString(), answer2?.ToString());
+		return (answer1.ToString(), answer2.ToString());
 	}
 }
