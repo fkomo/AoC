fe7e8a76d4a2cd536ea849925201392b5cbc5042
filip komorovsky <filip.komorovsky@gmail.com>
2024-11-29 15:46:59 +0100

    2018/07 input parse

--- AoC.App/2018/07_TheSumOfItsParts.cs
+++ AoC.App/2018/07_TheSumOfItsParts.cs
@@ -7,6 +7,18 @@ public class TheSumOfItsParts : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
+		var relations = input
+			.Select(x => (Prereq: x["Step ".Length], Id: x["Step F must be finished before step ".Length]))
+			.ToArray();
+
+		var steps = relations
+			.GroupBy(x => x.Id)
+			.ToDictionary(x => x.Key, x => x.Select(x => x.Prereq).ToArray());
+
+		var stepsWitNoPrereq = relations.Select(x => x.Prereq).Distinct().Where(x => !steps.ContainsKey(x));
+		foreach (var step in stepsWitNoPrereq)
+			steps.Add(step, []);
+
 		// TODO 2018/07
 
 		// part1
