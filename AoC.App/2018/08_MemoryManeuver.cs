0a8ab76fdcc128f5b4b001f840c6ff40564beb25
filip komorovsky <filip.komorovsky@gmail.com>
2025-02-13 14:04:47 +0100

    2018/08 p1

--- AoC.App/2018/08_MemoryManeuver.cs
+++ AoC.App/2018/08_MemoryManeuver.cs
@@ -1,20 +1,42 @@
 using Ujeby.AoC.Common;
+using Ujeby.Extensions;
 
 namespace Ujeby.AoC.App._2018_08;
 
-[AoCPuzzle(Year = 2018, Day = 08, Answer1 = null, Answer2 = null, Skip = false)]
+record struct Node(int[] Childs, int[] Meta);
+
+[AoCPuzzle(Year = 2018, Day = 08, Answer1 = "36027", Answer2 = null, Skip = false)]
 public class MemoryManeuver : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		// TODO 2018/08
+		var nums = input.Single().ToNumArray().Select(x => (int)x).ToArray();
+
+		var licenseTree = new Dictionary<int, Node>();
+		ProcessNode(nums, 0, licenseTree);
 
 		// part1
-		string answer1 = null;
+		var answer1 = licenseTree.Sum(x => x.Value.Meta.Sum());
 
 		// part2
 		string answer2 = null;
 
-		return (answer1?.ToString(), answer2?.ToString());
+		return (answer1.ToString(), answer2?.ToString());
+	}
+
+	static int ProcessNode(int[] licenseFile, int nodeId, Dictionary<int, Node> licenseTree)
+	{
+		var childs = new List<int>();
+
+		var next = nodeId + 2;
+		for (var i = 0; i < licenseFile[nodeId]; i++)
+		{
+			childs.Add(next);
+			next = ProcessNode(licenseFile, next, licenseTree);
+		}
+
+		licenseTree.Add(nodeId, new Node([.. childs], licenseFile[next..(next + licenseFile[nodeId + 1])]));
+
+		return next + licenseFile[nodeId + 1];
 	}
 }
