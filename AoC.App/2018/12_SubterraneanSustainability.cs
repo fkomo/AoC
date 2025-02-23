d7ece13508d61261ef7befb6e0d3b06e512b2af1
filip komorovsky <filip.komorovsky@gmail.com>
2025-02-24 00:13:51 +0100

    2018/12 p2 refactoring

--- AoC.App/2018/12_SubterraneanSustainability.cs
+++ AoC.App/2018/12_SubterraneanSustainability.cs
@@ -12,85 +12,35 @@ public class SubterraneanSustainability : PuzzleBase
 		var rules = input.Skip(2).Select(x => new Rule(x[..5], x[^1] == '#')).ToArray();
 
 		// part1
-		var answer1 = Grow2([.. plants], rules);
+		var plantsAfter = Grow([.. plants], rules, out long left);
+		var answer1 = GetSum(plantsAfter, left);
 
 		// part2
-		var answer2 = Grow2([.. plants], rules, generations: 50_000);
+		plantsAfter = Grow([.. plants], rules, out left, generations: 5_000);
+		
+		// generations 5_000, 50_000, 500_000, ... produces the same plant configuration
+		// only thery are moved further to the right - left is increasing by 10x (45_000, 450_000, 4_500_000, ...)
+		var gen = 5_000L;
+		var leftInc = 4_500L;
+		while (gen != 50_000_000_000)
+		{
+			gen *= 10;
+			leftInc *= 10;
+			left += leftInc;
+		}
 
-		// 000000000
-		// 0000000000
-		// 640ms
+		var answer2 = GetSum(plantsAfter, left);
 
 		return (answer1.ToString(), answer2.ToString());
 	}
 
-	static long Grow2(char[] plants, Rule[] rules, long generations = 20)
+	static string Grow(char[] plants, Rule[] rules, out long left, long generations = 20)
 	{
-		var grown = new HashSet<long>();
-		for (var i = 0; i < plants.Length; i++)
-			if (plants[i] == '#')
-				grown.Add(i);
-
-		var emptyToPlant = rules.Where(x => x.Grow && x.Pattern[2] == '.').ToArray();
-		var plantToPlant = rules.Where(x => x.Grow && x.Pattern[2] == '#').ToArray();
-
-		var possiblePlants = new int[] { -2, -1, 1, 2 };
-
-		for (var g = 0; g < generations; g++)
-		{
-			var grown2 = new List<long>();
-
-			foreach (var p in grown)
-			{
-				foreach (var r in plantToPlant)
-					if (MatchPattern(r.Pattern, grown, p))
-						grown2.Add(p);
-			}
-
-			var empty = new HashSet<long>();
-			foreach (var p in grown)
-			{
-				foreach (var pp in possiblePlants)
-					if (!grown.Contains(pp + p))
-						empty.Add(pp + p);
-			}
-
-			foreach (var p in empty)
-			{
-				foreach (var r in emptyToPlant)
-					if (MatchPattern(r.Pattern, grown, p))
-						grown2.Add(p);
-			}
-
-			grown = [.. grown2];
-		}
-
-		return grown.Sum();
-	}
-
-	static bool MatchPattern(string pattern, HashSet<long> grown, long p)
-	{
-		for (var i = 0; i < pattern.Length; i++)
-		{
-			if (pattern[i] == '#' && !grown.Contains(p - 2 + i))
-				return false;
-
-			if (pattern[i] == '.' && grown.Contains(p - 2 + i))
-				return false;
-		}
-
-		return true;
-	}
-
-	static long Grow(char[] plants, Rule[] rules, long generations = 20)
-	{
-		var left = 0;
+		left = 0;
 
 		var extPlants = Enumerable.Repeat('.', 5);
 		for (var g = 0; g < generations; g++)
 		{
-			//Debug.Line($"{g}: {string.Join("", plants)}");
-
 			// extend to left and right
 			plants = extPlants.Concat(plants).Concat(extPlants).ToArray();
 			left -= extPlants.Count();
@@ -99,14 +49,9 @@ public class SubterraneanSustainability : PuzzleBase
 			var plants2 = Enumerable.Repeat('.', plants.Length).ToArray();
 			for (var p = 2; p < plants.Length - 2; p++)
 			{
-				foreach (var rule in rules)
-				{
-					if (MatchPatternAt(plants, p, rule.Pattern))
-					{
-						plants2[p] = rule.Grow ? '#' : '.';
-						break;
-					}
-				}
+				var rule = rules.SingleOrDefault(x => MatchPatternAt(plants, p, x.Pattern));
+				if (rule != null)
+					plants2[p] = rule.Grow ? '#' : '.';
 			}
 
 			// trim left
@@ -119,8 +64,14 @@ public class SubterraneanSustainability : PuzzleBase
 			plants = plants[..(last + 1)];
 		}
 
-		Log.Line(string.Join("", plants));
+		Debug.Line($"gen={generations} left={left}");
+		Debug.Line(string.Join("", plants));
 
+		return string.Join("", plants);
+	}
+
+	static long GetSum(string plants, long left)
+	{
 		var sum = 0L;
 		for (var i = 0; i < plants.Length; i++)
 			if (plants[i] == '#')
@@ -139,4 +90,4 @@ public class SubterraneanSustainability : PuzzleBase
 	}
 }
 
-record struct Rule(string Pattern, bool Grow);
+record class Rule(string Pattern, bool Grow);
