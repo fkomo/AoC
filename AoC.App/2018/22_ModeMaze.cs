18dfb1a4b661fd48c50b7aa26c52ccf5699511e1
filip komorovsky <filip.komorovsky@gmail.com>
2025-03-13 15:20:24 +0100

    2018/22 p2

--- AoC.App/2018/22_ModeMaze.cs
+++ AoC.App/2018/22_ModeMaze.cs
@@ -1,63 +1,130 @@
 using Ujeby.AoC.Common;
 using Ujeby.Extensions;
 using Ujeby.Vectors;
-using ErosionMap = System.Collections.Generic.Dictionary<Ujeby.Vectors.v2i, (long erosionLevel, int regionType)>;
+using ErosionMap = System.Collections.Generic.Dictionary<Ujeby.Vectors.v2i, (long ErosionLevel, Ujeby.AoC.App._2018_22.Region RegionType)>;
 
 namespace Ujeby.AoC.App._2018_22;
 
-[AoCPuzzle(Year = 2018, Day = 22, Answer1 = "8681", Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2018, Day = 22, Answer1 = "8681", Answer2 = "1070", Skip = true)]
 public class ModeMaze : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
 		var depth = input[0].ToNumArray()[0];
+
+		var start = v2i.Zero;
 		var target = new v2i(input[1].ToNumArray());
-		var area = new aab2i(v2i.Zero, target);
+		var mapArea = new aab2i(start, target);
 
 		// part1
-		var erosionLevels = new ErosionMap();
-		foreach (var a in area.EnumPoints())
-			erosionLevels.GetOrAdd(a, target, depth);
-		//erosionLevels.GetOrAdd(target + v2i.Down, target, depth);
-		//erosionLevels.GetOrAdd(target + v2i.Left, target, depth);
+		var erosionMap = new ErosionMap();
+		foreach (var a in mapArea.EnumPoints())
+			erosionMap.Get(a, target, depth, out _, out _);
 
-		var answer1 = erosionLevels.Sum(x => x.Value.regionType);
+		var answer1 = erosionMap.Sum(x => (int)x.Value.RegionType);
 
 		// part2
-		//erosionLevels.Print();
+		// TODO 2018/22 OPTIMIZE p2 ~6min
+		var answer2 = ShortestPath(erosionMap, mapArea.Add(v2i.Zero, new v2i(38, 0)), start, target, depth);
 
-		string answer2 = null;
-
-		return (answer1.ToString(), answer2?.ToString());
+		return (answer1.ToString(), answer2.ToString());
 	}
+
+	static int ShortestPath(ErosionMap erosionMap, aab2i mapArea, v2i start, v2i target, long depth)
+	{
+		var bestToTargetSoFar = int.MaxValue;
+
+		var best = new Dictionary<(v2i, Tool), int>();
+		
+		var queue = new Queue<(v2i p, int timeTaken, Tool tool, HashSet<v2i> visited)>();
+		queue.Enqueue((start, 0, Tool.Torch, [v2i.Zero]));
+
+		while (queue.Count > 0)
+		{
+			var (p, timeTaken, tool, visited) = queue.Dequeue();
+
+			if (best.TryGetValue((p, tool), out int bestTime) && bestTime <= timeTaken)
+				continue;
+
+			if (timeTaken >= bestToTargetSoFar)
+				continue;
+
+			best[(p, tool)] = timeTaken;
+
+			if (p == target)
+			{
+				if (tool != Tool.Torch && (!best.TryGetValue((target, Tool.Torch), out bestTime) || bestTime >= timeTaken + 7))
+					best[(p, Tool.Torch)] = timeTaken + 7;
+
+				bestToTargetSoFar = best[(p, Tool.Torch)];
+				continue;
+			}
+
+			erosionMap.Get(p, target, depth, out _, out Region pRegion);
+			foreach (var tool2 in _toolNeeded[pRegion])
+			{
+				var timeTaken2 = (tool2 == tool) ? timeTaken + 1 : timeTaken + 8;
+
+				foreach (var d in v2i.UpDownLeftRight)
+				{
+					var p2 = p + d;
+					if (!mapArea.Contains(p2) || visited.Contains(p2))
+						continue;
+
+					erosionMap.Get(p2, target, depth, out _, out Region p2Region);
+					if (!_regionsAllowed[tool2].Contains(p2Region))
+						continue;
+
+					var hs2 = visited.ToHashSet();
+					hs2.Add(p2);
+
+					queue.Enqueue((p2, timeTaken2, tool2, hs2));
+				}
+			}
+		}
+
+		return best[(target, Tool.Torch)];
+	}
+
+	readonly static Dictionary<Region, Tool[]> _toolNeeded = new()
+	{
+		{ Region.Rocky, [Tool.Torch, Tool.ClimbingGear] },
+		{ Region.Wet, [Tool.ClimbingGear, Tool.Neither] },
+		{ Region.Narrow, [Tool.Torch, Tool.Neither] }
+	};
+
+	readonly static Dictionary<Tool, Region[]> _regionsAllowed = new()
+	{
+		{ Tool.Torch, [Region.Rocky, Region.Narrow] },
+		{ Tool.ClimbingGear, [Region.Rocky, Region.Wet] },
+		{ Tool.Neither, [Region.Wet, Region.Narrow] }
+	};
+}
+
+enum Region : int
+{
+	Rocky = 0,
+	Wet = 1,
+	Narrow = 2
+}
+
+enum Tool : int
+{
+	Neither = 0,
+	Torch,
+	ClimbingGear
 }
 
 static class Extensions
 {
-	public static void Print(this ErosionMap erosionMap)
+	public static void Get(this ErosionMap erosionMap, v2i a, v2i target, long depth, out long erosionLevel, out Region region)
 	{
-#if DEBUG
-		static char GetRegionType(long t) => t switch
+		if (erosionMap.TryGetValue(a, out (long ErosionLevel, Region Region) value))
 		{
-			0 => '.',
-			1 => '=',
-			2 => '|',
-			_ => '\0',
-		};
-	
-		var area = aab2i.FromPoints(erosionMap.Keys);
-
-		foreach (var line in erosionMap.GroupBy(x => x.Key.Y).OrderBy(x => x.Key))
-			Debug.Line(string.Join("", line.OrderBy(x => x.Key.X).Select(x => GetRegionType(x.Value.regionType))));
-
-		Debug.Line();
-#endif
-	}
-
-	public static long GetOrAdd(this ErosionMap erosionMap, v2i a, v2i target, long depth)
-	{
-		if (erosionMap.TryGetValue(a, out (long erosionLevel, int regionType) value))
-			return value.erosionLevel;
+			erosionLevel = value.ErosionLevel;
+			region = value.Region;
+			return;
+		}
 
 		long geoIdx;
 		if (a == v2i.Zero || a == target)
@@ -69,23 +136,23 @@ static class Extensions
 		else
 		{
 			long left;
-			if (erosionMap.TryGetValue(a + v2i.Left, out (long erosionLevel, int regionType) leftValue))
-				left = leftValue.erosionLevel;
+			if (erosionMap.TryGetValue(a + v2i.Left, out (long ErosionLevel, Region Region) leftValue))
+				left = leftValue.ErosionLevel;
 			else
-				left = erosionMap.GetOrAdd(a + v2i.Left, target, depth);
+				erosionMap.Get(a + v2i.Left, target, depth, out left, out _);
 
 			long up;
-			if (erosionMap.TryGetValue(a + v2i.Down, out (long erosionLevel, int regionType) upValue))
-				up = upValue.erosionLevel;
+			if (erosionMap.TryGetValue(a + v2i.Down, out (long ErosionLevel, Region Region) upValue))
+				up = upValue.ErosionLevel;
 			else
-				up = erosionMap.GetOrAdd(a + v2i.Down, target, depth);
+				erosionMap.Get(a + v2i.Down, target, depth, out up, out _);
 
 			geoIdx = left * up;
 		}
 
-		var erosionLevel = (geoIdx + depth) % 20183;
-		erosionMap.Add(a, (erosionLevel, (int)(depth + erosionLevel) % 3));
+		erosionLevel = (geoIdx + depth) % 20183;
+		region = (Region)((depth + erosionLevel) % 3);
 
-		return erosionLevel;
+		erosionMap.Add(a, (erosionLevel, region));
 	}
 }
