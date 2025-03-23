c04b8751b15113d67e6ab4f58e2a3a5a6df98cae
filip komorovsky <filip.komorovsky@gmail.com>
2025-03-23 16:11:29 +0100

    2018/23 p1

--- AoC.App/2018/23_ExperimentalEmergencyTeleportation.cs
+++ AoC.App/2018/23_ExperimentalEmergencyTeleportation.cs
@@ -1,20 +1,26 @@
 using Ujeby.AoC.Common;
+using Ujeby.Extensions;
+using Ujeby.Vectors;
 
 namespace Ujeby.AoC.App._2018_23;
 
-[AoCPuzzle(Year = 2018, Day = 23, Answer1 = null, Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2018, Day = 23, Answer1 = "691", Answer2 = null, Skip = false)]
 public class ExperimentalEmergencyTeleportation : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		// TODO 2018/23
+		var nanobots = input
+			.Select(x => x.ToNumArray())
+			.Select(x => new { Pos = new v3i([.. x.Take(3)]), Radius = x[3] })
+			.ToArray();
 
 		// part1
-		string answer1 = null;
+		var largest = nanobots.OrderByDescending(x => x.Radius).First();
+		var answer1 = nanobots.Count(x => v3i.ManhDistance(x.Pos, largest.Pos) <= largest.Radius);
 
 		// part2
 		string answer2 = null;
 
-		return (answer1?.ToString(), answer2?.ToString());
+		return (answer1.ToString(), answer2?.ToString());
 	}
 }
