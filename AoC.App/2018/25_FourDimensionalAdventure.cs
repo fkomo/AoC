6218fac6f60dcb687e67cb7b8e9f118c292bfe3c
filip komorovsky <filip.komorovsky@gmail.com>
2025-04-06 15:24:42 +0200

    2018/25

--- AoC.App/2018/25_FourDimensionalAdventure.cs
+++ AoC.App/2018/25_FourDimensionalAdventure.cs
@@ -1,20 +1,49 @@
 using Ujeby.AoC.Common;
+using Ujeby.Extensions;
+using Ujeby.Vectors;
 
 namespace Ujeby.AoC.App._2018_25;
 
-[AoCPuzzle(Year = 2018, Day = 25, Answer1 = null, Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2018, Day = 25, Answer1 = "388", Answer2 = "*", Skip = false)]
 public class FourDimensionalAdventure : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		// TODO 2018/25
+		var constellations = input
+			.Select(x => new List<v4i> { new(x.ToNumArray()) })
+			.ToArray();
 
 		// part1
-		string answer1 = null;
+		var noChange = false;
+		while (!noChange)
+		{
+			noChange = true;
+
+			for (var i = 0; i < constellations.Length; i++)
+			{
+				if (constellations[i].Count == 0)
+					continue;
+
+				foreach (var nIdx in constellations
+					.Select((x, i) => (x, i))
+					.Where(x => x.i != i && x.x.Any(xx => constellations[i].Any(xxx => v4i.ManhDistance(xx, xxx) <= 3)))
+					.Select(x => x.i))
+				{
+					constellations[i].AddRange(constellations[nIdx]);
+					constellations[nIdx].Clear();
+
+					noChange = false;
+				}
+			}
+
+			constellations = [.. constellations.Where(x => x.Count > 0)];
+		}
+
+		var answer1 = constellations.Length;
 
 		// part2
-		string answer2 = null;
+		var answer2 = "*";
 
-		return (answer1?.ToString(), answer2?.ToString());
+		return (answer1.ToString(), answer2.ToString());
 	}
 }
