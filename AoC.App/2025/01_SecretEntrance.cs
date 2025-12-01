2cb32c4cd3d6993228f31c58554f3d902e2cf7e6
filip komorovsky <filip.komorovsky@gmail.com>
2025-12-01 23:37:40 +0100

    2025/01 p2

--- AoC.App/2025/01_SecretEntrance.cs
+++ AoC.App/2025/01_SecretEntrance.cs
@@ -2,28 +2,38 @@ using Ujeby.AoC.Common;
 
 namespace Ujeby.AoC.App._2025_01;
 
-[AoCPuzzle(Year = 2025, Day = 01, Answer1 = "1177", Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2025, Day = 01, Answer1 = "1177", Answer2 = "6768", Skip = false)]
 public class SecretEntrance : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
 		var rotations = input
-			.Select(x => long.Parse(x.Replace('L', '-').TrimStart('R')))
+			.Select(x => int.Parse(x.Replace('L', '-').TrimStart('R')))
 			.ToArray();
 
 		// part1
 		var dial = 50L;
-		var answer1 = 0L;
-		for (var i = 0; i < rotations.Length; i++)
+		var answer1 = 0;
+		foreach (var rot in rotations)
 		{
-			dial = (dial + rotations[i] + 100) % 100;
+			dial = (100 + dial + rot) % 100;
 			if (dial == 0)
 				answer1++;
 		}
 
 		// part2
-		string answer2 = null;
+		dial = 50L;
+		var answer2 = 0;
+		foreach (var rot in rotations)
+		{
+			for (int r = 0, rot1 = rot > 0 ? 1 : -1; r < System.Math.Abs(rot); r++)
+			{
+				dial = (100 + dial + rot1) % 100;
+				if (dial == 0)
+					answer2++;
+			}
+		}
 
-		return (answer1.ToString(), answer2?.ToString());
+		return (answer1.ToString(), answer2.ToString());
 	}
 }
