6a757550527a4d7c751e038e29121fc468ddc662
filip komorovsky <filip.komorovsky@gmail.com>
2025-12-04 00:28:26 +0100

    2025/03 p2 in progress

--- AoC.App/2025/03_Lobby.cs
+++ AoC.App/2025/03_Lobby.cs
@@ -7,18 +7,22 @@ public class Lobby : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		var banks = input.Select(x => x.Select(x => x - '0').ToArray()).ToArray();
+		var banks = input
+			.Select(x => x
+				.Select(xx => xx - '0')
+				.ToArray())
+			.ToArray();
 
 		// part1
-		var answer1 = banks.Sum(GetJoltage);
+		var answer1 = banks.Sum(Get2BatteryJoltage);
 
 		// part2
-		string answer2 = null;
+		long? answer2 = null; // banks.Sum(x => GetNBatteryJoltage(x, batteryCount: 12));
 
 		return (answer1.ToString(), answer2?.ToString());
 	}
 
-	static int GetJoltage(int[] bank)
+	static int Get2BatteryJoltage(int[] bank)
 	{
 		var first = -1;
 		var second = -1;
@@ -42,5 +46,32 @@ public class Lobby : PuzzleBase
 			second = bank[^1];
 
 		return 10 * first + second;
+	}
+
+	static long GetNBatteryJoltage(int[] bank, int batteryCount = 2)
+	{
+		var batteries = Enumerable.Repeat(-1, batteryCount).ToArray();
+
+		for (var bankIdx = 0; bankIdx < bank.Length; bankIdx++)
+		{
+			for (var batteryIdx = 0; batteryIdx < batteryCount; batteryIdx++)
+			{
+				if (bank[bankIdx] > batteries[batteryIdx])
+				{
+					batteries[batteryIdx] = bank[bankIdx];
+					for (var i = batteryIdx + 1; i < batteries.Length; i++)
+						batteries[i] = -1;
+
+					break;
+				}
+			}
+		}
+
+		var joltage = 0L;
+		var n = 1L;
+		for (int i = batteryCount - 1; i >= 0; i--, n *= 10)
+			joltage += batteries[i] * n;
+
+		return joltage;
 	}
 }
