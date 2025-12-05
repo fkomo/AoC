a7dac526f68fa644ebe0ebea09a8cc57c4557f90
filip komorovsky <filip.komorovsky@gmail.com>
2025-12-05 09:41:20 +0100

    2025/04 p2

--- AoC.App/2025/04_PrintingDepartment.cs
+++ AoC.App/2025/04_PrintingDepartment.cs
@@ -4,21 +4,48 @@ using Ujeby.Vectors;
 
 namespace Ujeby.AoC.App._2025_04;
 
-[AoCPuzzle(Year = 2025, Day = 04, Answer1 = "1424", Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2025, Day = 04, Answer1 = "1424", Answer2 = "8727", Skip = false)]
 public class PrintingDepartment : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
 		var map = input.Select(x => x.ToArray()).ToArray();
+		var allPapers = map.EnumAll('@').ToArray();
 
 		// part1
-		var answer1 = map
-			.EnumAll('@')
+		var answer1 = allPapers
 			.Count(x => v2i.PlusMinusOne.Count(xx => map.TryGet(xx + x, out char neighbour) && neighbour == '@') < 4);
 
 		// part2
-		string answer2 = null;
+		var papers = allPapers
+			.ToDictionary(x => x, x => v2i.PlusMinusOne.Count(xx => map.TryGet(xx + x, out char neighbour) && neighbour == '@'));
+		
+		var initialCount = papers.Count;
 
-		return (answer1.ToString(), answer2?.ToString());
+		var toRemove = papers.Where(x => x.Value < 4).ToArray();
+		while (toRemove.Length > 0)
+		{
+			foreach (var rem in toRemove)
+			{
+				papers.Remove(rem.Key);
+
+				// update neighbours
+				foreach (var n in v2i.PlusMinusOne.Select(x => x + rem.Key))
+				{
+					if (!papers.ContainsKey(n))
+						continue;
+
+					papers[n]--;
+					if (papers[n] <= 0)
+						papers.Remove(n);
+				}
+			}
+
+			toRemove = [.. papers.Where(x => x.Value < 4)];
+		}
+
+		var answer2 = initialCount - papers.Count;
+
+		return (answer1.ToString(), answer2.ToString());
 	}
 }
