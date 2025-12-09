3b414832784c488fd95500cf449a5723c1f0ae5b
filip komorovsky <filip.komorovsky@gmail.com>
2025-12-10 00:51:33 +0100

    2025/08 p1+p2

---
+++ AoC.App/2025/07_Laboratories.cs
@@ -0,0 +1,20 @@
+using Ujeby.AoC.Common;
+
+namespace Ujeby.AoC.App._2025_07;
+
+[AoCPuzzle(Year = 2025, Day = 07, Answer1 = null, Answer2 = null, Skip = false)]
+public class Laboratories : PuzzleBase
+{
+	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
+	{
+		// TODO 2025/07
+
+		// part1
+		string answer1 = null;
+
+		// part2
+		string answer2 = null;
+
+		return (answer1?.ToString(), answer2?.ToString());
+	}
+}
---
+++ AoC.App/2025/09_MovieTheater.cs
@@ -0,0 +1,20 @@
+using Ujeby.AoC.Common;
+
+namespace Ujeby.AoC.App._2025_09;
+
+[AoCPuzzle(Year = 2025, Day = 09, Answer1 = null, Answer2 = null, Skip = false)]
+public class MovieTheater : PuzzleBase
+{
+	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
+	{
+		// TODO 2025/09
+
+		// part1
+		string answer1 = null;
+
+		// part2
+		string answer2 = null;
+
+		return (answer1?.ToString(), answer2?.ToString());
+	}
+}
