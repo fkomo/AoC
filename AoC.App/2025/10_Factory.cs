2b0f25301ec787f535d868d8ee4646c94810d2eb
fkomo <fkomo@buchanica3>
2025-12-27 16:19:59 +0100

    2025/10 input parse

--- AoC.App/2025/10_Factory.cs
+++ AoC.App/2025/10_Factory.cs
@@ -7,10 +7,17 @@ public class Factory : PuzzleBase
 {
 	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
 	{
-		// TODO 2025/10
+		var machines = input
+			.Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+			.Select(x => (
+				LightDiagram: x[0].Trim('[', ']'), 
+				Buttons: x[1..^1].Select(xx => xx.Trim(')', '(').Split(',').Select(xxx => int.Parse(xxx)).ToArray()).ToArray(),
+				Joltage: x[^1].Trim('{', '}').Split(',').Select(xx => int.Parse(xx)).ToArray())
+				)
+			.ToArray();
 
-		// part1
-		string answer1 = null;
+        // part1
+        string answer1 = null;
 
 		// part2
 		string answer2 = null;
--- AoC.App/2025/11_Reactor.cs
+++ AoC.App/2025/11_Reactor.cs
@@ -10,48 +10,77 @@ public class Reactor : PuzzleBase
 		var devices = input.ToDictionary(x => x[..3], x => x[5..].Split(' '));
 
 		// part1
-		var paths = new HashSet<string>();
-		var queue = new Queue<List<string>>();
-		
-		queue.Enqueue(["you"]);
+		var answer1 = devices.AllPaths("you", "out").Count;
 
-		while (queue.Count > 0)
+		// part2
+		//var svr_dac = devices.AllPaths("svr", "dac"); // too long
+		//var dac_fft = devices.AllPaths("dac", "fft"); // 0
+		//var fft_out = devices.AllPaths("fft", "out"); // too long
+		//var svr_fft = devices.AllPaths("svr", "fft"); // too long
+		//var fft_dac = devices.AllPaths("fft", "dac"); // too long
+
+		//var dac_out = devices.AllPaths("dac", "out"); // 25644
+
+		// svr|...|fft|...|dac|...|out paths
+		//var answer2 = devices.AllPaths("svr", "fft", "dac").Count;
+		var answer2 = 0;
+
+        return (answer1.ToString(), answer2.ToString());
+	}
+}
+
+static class Extensions
+{
+	public static HashSet<string[]> AllPaths(this Dictionary<string, string[]> devices, params string[] nodes)
+	{
+		var paths = new HashSet<string[]>();
+
+		//var queue = new Queue<List<string>>();
+		var stack = new Stack<List<string>>();
+
+		//queue.Enqueue([nodes[0]]);
+		stack.Push([nodes[0]]);
+
+		//while (queue.Count > 0)
+		while (stack.Count > 0)
 		{
-			var path = queue.Dequeue();
+			var path = stack.Pop();
+			//var path = queue.Dequeue();
 
 			while (true)
 			{
 				var last = path.Last();
-				if (last == "out")
-				{
-					var p = string.Join('|', path);
-					Debug.Line(p);
 
-					paths.Add(p);
+				if (path.Count > 1 && nodes.Length > 2 && path.Contains(nodes[2]) && !path.Contains(nodes[1]))
 					break;
-				}	
+	
+				if (last == nodes[^1])
+				{
+					if (nodes.Any(x => !path.Contains(x)))
+						break;
 
+					paths.Add([.. path]);
+					break;
+				}
+
+				// maybe not needed check
 				if (!devices.ContainsKey(last))
 					break;
 
-				if (devices[last].Length == 1)
+				if (devices[last].Length == 1 && !path.Contains(devices[last][0]))
 				{
 					path.Add(devices[last][0]);
 					continue;
 				}
 
-				foreach (var next in devices[last])
-					queue.Enqueue([.. path, next]);
+				foreach (var next in devices[last].Where(x => !path.Contains(x)))
+					//queue.Enqueue([.. path, next]);
+					stack.Push([.. path, next]);
 
 				break;
 			}
 		}
 
-		var answer1 = paths.Count;
-
-		// part2
-		string answer2 = null;
-
-		return (answer1.ToString(), answer2?.ToString());
+		return paths;
 	}
 }
