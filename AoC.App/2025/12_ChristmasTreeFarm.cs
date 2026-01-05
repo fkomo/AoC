0f303d68b15029ea790e661c23b9ad9e88213b25
fkomo <fkomo@buchanica3>
2026-01-05 11:19:34 +0100

    2025/12 p1+p2

--- AoC.App/2025/12_ChristmasTreeFarm.cs
+++ AoC.App/2025/12_ChristmasTreeFarm.cs
@@ -1,20 +1,56 @@
 using Ujeby.AoC.Common;
+using Ujeby.Extensions;
+using Ujeby.Vectors;
 
 namespace Ujeby.AoC.App._2025_12;
 
-[AoCPuzzle(Year = 2025, Day = 12, Answer1 = null, Answer2 = null, Skip = false)]
+[AoCPuzzle(Year = 2025, Day = 12, Answer1 = "448", Answer2 = "*", Skip = false)]
 public class ChristmasTreeFarm : PuzzleBase
 {
-	protected override (string Part1, string Part2) SolvePuzzle(string[] input)
-	{
-		// TODO 2025/12
+    protected override (string Part1, string Part2) SolvePuzzle(string[] input)
+    {
+        var presents = input
+            .Split(string.Empty)[..^1]
+            .Select(x => x[1..])
+            .Select(x => x.Select(xx => xx.ToArray()).ToArray())
+            .ToArray();
 
-		// part1
-		string answer1 = null;
+        var presentAreas = presents
+            .Select(x => x.Sum(xx => xx.Count(xxx => xxx == '#')))
+            .ToArray();
 
-		// part2
-		string answer2 = null;
+        var regions = input
+            .Split(string.Empty)[^1]
+            .Select(x => x.Split(':'))
+            .Select(x => (
+                size: new v2i(x[0].ToNumArray()),
+                presCount: x[1].ToNumArray()))
+            .ToArray();
 
-		return (answer1?.ToString(), answer2?.ToString());
-	}
+        Debug.Line($"{regions.Length} regions");
+
+        // part1
+        var answer1 = 0L;
+        foreach (var (size, presCount) in regions)
+        {
+            // area big enough to fit presents with no need to overlap
+            if (size.X / 3 * size.Y / 3 >= presCount.Sum())
+            {
+                answer1++;
+                continue;
+            }
+
+            // area too small to fit presents even if perfectly overlapped
+            else if (size.Area() < presCount.Select((x, i) => x * presentAreas[i]).Sum())
+                continue;
+
+            else
+                throw new NotImplementedException();
+        }
+
+        // part2
+        string answer2 = "*";
+
+        return (answer1.ToString(), answer2.ToString());
+    }
 }
